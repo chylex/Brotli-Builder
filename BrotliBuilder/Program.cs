c08a4ed9530cbb10370a44859a83ed0efedef7b0
chylex <contact@chylex.com>
2018-11-14 20:19:47 +0100

    Push Visual Studio project files

---
+++ BrotliBuilder/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Windows.Forms;
+
+namespace BrotliBuilder{
+    static class Program{
+        [STAThread]
+        private static void Main(){
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+}
