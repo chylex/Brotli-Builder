6469cacc42bc48c5fa6fde2ba40e79924face228
chylex <contact@chylex.com>
2018-11-16 14:03:04 +0100

    Add boilerplate for testing elements of Brotli format

---
+++ UnitTests/Brotli/TestElements.fs
@@ -0,0 +1,13 @@
+﻿namespace UnitTests.Brotli.TestElements
+
+open Xunit
+open System
+open BrotliLib.IO
+
+
+module Helper =
+    let convert (obj: 'a) (context: 'b) (serializer: IBitSerializer<'a, 'b>) =
+        let stream = BitStream()
+        serializer.ToBits(stream.GetWriter(), obj, context)
+        serializer.FromBits(stream.GetReader(), context)
+
