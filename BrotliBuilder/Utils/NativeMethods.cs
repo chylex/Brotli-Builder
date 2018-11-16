2c140680b38c389abb7f56b047da96434c54ac91
chylex <contact@chylex.com>
2018-11-16 22:58:38 +0100

    Add NativeMethods w/ method to force plaintext in a RichTextBox

---
+++ BrotliBuilder/Utils/NativeMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace BrotliBuilder.Utils{
+    static class NativeMethods{
+        public const int EM_SETTEXTMODE = 0x459;
+        public const int TM_PLAINTEXT = 1;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        public static void SetPlainTextMode(this RichTextBox textBox){
+            SendMessage(textBox.Handle, EM_SETTEXTMODE, new IntPtr(TM_PLAINTEXT), IntPtr.Zero);
+        }
+    }
+}
