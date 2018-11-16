abff015bcfc81232ef8688aa22393e8b0874e1b8
chylex <contact@chylex.com>
2018-11-16 14:47:14 +0100

    Implement Brotli meta-block data length serialization

---
+++ BrotliLib/Brotli/Components/Header/DataLength.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using BrotliLib.IO;
+
+namespace BrotliLib.Brotli.Components.Header{
+    /// <summary>
+    /// Describes size of uncompressed data stored in a meta-block.
+    /// https://tools.ietf.org/html/rfc7932#section-9.2
+    /// </summary>
+    public sealed class DataLength{
+        private const int MinNibbles = 4;
+        private const int MaxNibbles = 6;
+
+        public static DataLength Empty = new DataLength(0, 0);
+
+        private static int CalculateNibblesRequired(int uncompressedBytes){
+            if (uncompressedBytes < 0){
+                throw new ArgumentOutOfRangeException(nameof(uncompressedBytes), "The amount of bytes must be at least 0.");
+            }
+            else if (uncompressedBytes == 0){
+                return 0;
+            }
+
+            for(int nibbles = MinNibbles; nibbles <= MaxNibbles; nibbles++){
+                int maxValue = 1 << (4 * nibbles);
+
+                if (uncompressedBytes <= maxValue){
+                    return nibbles;
+                }
+            }
+
+            throw new ArgumentOutOfRangeException(nameof(uncompressedBytes), "The amount of bytes (" + uncompressedBytes + ") cannot be expressed with at most " + MaxNibbles + " nibbles.");
+        }
+
+        // Data
+
+        public int ChunkNibbles { get; }
+        public int UncompressedBytes { get; }
+
+        private DataLength(int chunkNibbles, int uncompressedBytes){
+            this.ChunkNibbles = chunkNibbles;
+            this.UncompressedBytes = uncompressedBytes;
+        }
+
+        public DataLength(int uncompressedBytes) : this(CalculateNibblesRequired(uncompressedBytes), uncompressedBytes){}
+
+        public override int GetHashCode(){
+            return UncompressedBytes;
+        }
+
+        public override bool Equals(object obj){
+            return obj is DataLength other && other.ChunkNibbles == ChunkNibbles && other.UncompressedBytes == UncompressedBytes;
+        }
+
+        // Serialization
+
+        public static readonly IBitSerializer<DataLength, object> Serializer = new BitSerializer<DataLength, object>(
+            fromBits: (reader, context) => {
+                int chunkNibbles;
+		        
+                switch(reader.NextChunk(2)){
+                    case 0b00: chunkNibbles = 4; break;
+                    case 0b01: chunkNibbles = 5; break;
+                    case 0b10: chunkNibbles = 6; break;
+                    case 0b11: chunkNibbles = 0; break;
+                    default: throw new InvalidOperationException("Reading two bits somehow returned a value outside [0, 3].");
+                }
+
+                int uncompressedBytes = (chunkNibbles == 0) ? 0 : 1 + reader.NextChunk(4 * chunkNibbles);
+
+                return new DataLength(chunkNibbles, uncompressedBytes);
+            },
+
+            toBits: (writer, obj, context) => {
+                switch(obj.ChunkNibbles){
+                    case 4: writer.WriteChunk(2, 0b00); break;
+                    case 5: writer.WriteChunk(2, 0b01); break;
+                    case 6: writer.WriteChunk(2, 0b10); break;
+                    case 0: writer.WriteChunk(2, 0b11); break;
+                    default: throw new InvalidOperationException("Data length object has an invalid amount of nibbles: " + obj.ChunkNibbles);
+                }
+
+                writer.WriteChunk(4 * obj.ChunkNibbles, obj.UncompressedBytes - 1);
+            }
+        );
+    }
+}
--- UnitTests/Brotli/TestElements.fs
+++ UnitTests/Brotli/TestElements.fs
@@ -4,6 +4,7 @@ open Xunit
 open System
 open BrotliLib.IO
 open BrotliLib.Brotli.Components
+open BrotliLib.Brotli.Components.Header
 
 
 module Helper =
@@ -27,3 +28,39 @@ module WindowSize =
     [<InlineData(25)>]
     let ``constructing window size with invalid amount of bits throws exception`` (wbits: int) =
         Assert.Throws<ArgumentOutOfRangeException>(fun () -> WindowSize(wbits) |> ignore)
+
+
+module DataLength =
+    let values = seq {
+        yield! seq { 0..65536 } // all values that fit into 0-4 nibbles
+        yield 65537    // 5 nibbles
+        yield 1048576  // 5 nibbles
+        yield 1048577  // 6 nibbles
+        yield 16777216 // 6 nibbles
+    }
+
+    [<Fact>]
+    let ``converting to and from bits yields same object`` () =
+        for bytes in values do // ReSharper doesn't play nice with enumeration disabled
+            let dl = DataLength(bytes)
+            Assert.Equal(dl, Helper.convert dl null DataLength.Serializer)
+
+    [<Theory>]
+    [<InlineData(0, 0)>]
+    [<InlineData(1, 4)>]
+    [<InlineData(16, 4)>]
+    [<InlineData(256, 4)>]
+    [<InlineData(4096, 4)>]
+    [<InlineData(65536, 4)>]
+    [<InlineData(65537, 5)>]
+    [<InlineData(1048576, 5)>]
+    [<InlineData(1048577, 6)>]
+    [<InlineData(16777216, 6)>]
+    let ``different lengths use the fewest nibbles necessary`` (bytes: int, nibbles: int) =
+        Assert.Equal(nibbles, DataLength(bytes).ChunkNibbles)
+
+    [<Theory>]
+    [<InlineData(-1)>]
+    [<InlineData(16777217)>]
+    let ``constructing data length with invalid amount of bytes throws exception`` (bytes: int) =
+        Assert.Throws<ArgumentOutOfRangeException>(fun () -> DataLength(bytes) |> ignore)
