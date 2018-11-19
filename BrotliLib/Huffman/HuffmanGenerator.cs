8d883a9b30a6576aa4bb12cf8d68de79563849a1
chylex <contact@chylex.com>
2018-11-19 13:09:52 +0100

    Add utility for generating Huffman trees w/ unit tests

---
+++ BrotliLib/Huffman/HuffmanGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BrotliLib.IO;
+
+namespace BrotliLib.Huffman{
+    /// <summary>
+    /// Provides utilities for generating Huffman trees.
+    /// </summary>
+    public static class HuffmanGenerator<T>{
+        /// <summary>
+        /// Pairs a symbol with the length of its path in a tree.
+        /// </summary>
+        public class Entry{
+            public T Symbol { get; }
+            public byte Bits { get; }
+
+            public Entry(T symbol, byte bits){
+                Symbol = symbol;
+                Bits = bits;
+            }
+        }
+
+        /// <summary>
+        /// Generates a Huffman tree from a mapping of symbols to the lengths of their paths in the tree.
+        /// If the list contains a single symbol with any length, a <see cref="HuffmanNode{T}.Leaf"/> node will be returned, that always returns the symbol without advancing the enumerator.
+        /// </summary>
+        /// <param name="entries">Ordered alphabet with each symbol mapped to its path length.</param>
+        /// <exception cref="ArgumentException">Thrown when the list of entries is empty or it contains 2+ symbols and at least one of them has a path length of 0, and when the described paths generate unreachable symbols.</exception>
+        public static HuffmanNode<T> GenerateFromBitCounts(IList<Entry> entries){
+            if (entries.Count == 1){
+                return new HuffmanNode<T>.Leaf(entries[0].Symbol);
+            }
+            else if (entries.Count == 0){
+                throw new ArgumentException("Cannot generate a Huffman tree with no symbols.");
+            }
+            else if (entries.Any(entry => entry.Bits == 0)){
+                throw new ArgumentException("Cannot generate a Huffman tree that contains multiple symbols, some of which have a length of 0.");
+            }
+
+            int maxBits = entries.Max(entry => entry.Bits);
+            var bitCounts = entries.GroupBy(entry => entry.Bits).ToDictionary(group => group.Key, group => group.Count());
+
+            int code = 0;
+            int[] nextCode = new int[maxBits];
+
+            for(byte bits = 1; bits <= maxBits; bits++){
+                code = (code + (bitCounts.TryGetValue((byte)(bits - 1), out int count) ? count : 0)) << 1;
+                nextCode[bits - 1] = code;
+            }
+
+            Dictionary<BitStream, T> symbolPaths = new Dictionary<BitStream, T>(bitCounts.Count);
+
+            foreach(Entry entry in entries){
+                int pathCode = nextCode[entry.Bits - 1]++;
+                symbolPaths[new BitStream(Convert.ToString(pathCode, 2).PadLeft(entry.Bits, '0'))] = entry.Symbol;
+            }
+
+            return GenerateFromSymbolPaths(symbolPaths);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="Dictionary{TKey, TValue}"/>, which maps <see cref="BitStream"/> paths to their assigned symbols, into a <see cref="HuffmanNode{T}"/>.
+        /// </summary>
+        /// <param name="paths">Mapping of bit paths to the symbols. All possible paths must terminate in a symbol.</param>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="paths"/> dictionary is missing a possible path, or a path is unreachable.</exception>
+        public static HuffmanNode<T> GenerateFromSymbolPaths(Dictionary<BitStream, T> paths){
+            int longestBranch = paths.Select(path => path.Key.Length).DefaultIfEmpty(0).Max();
+            int totalLeaves = 0;
+
+            HuffmanNode<T> GenerateNodeBranch(BitStream prefix, bool nextBit){
+                if (prefix.Length >= longestBranch){
+                    throw new ArgumentException("Incomplete symbol paths, leaking branch at: " + prefix, nameof(paths));
+                }
+
+                BitStream branch = prefix.Clone();
+                branch.Add(nextBit);
+                
+                if (paths.TryGetValue(branch, out T symbol)){
+                    ++totalLeaves;
+                    return new HuffmanNode<T>.Leaf(symbol);
+                }
+                else{
+                    return GenerateNode(branch);
+                }
+            }
+
+            HuffmanNode<T> GenerateNode(BitStream stream){
+                return new HuffmanNode<T>.Path(GenerateNodeBranch(stream, false), GenerateNodeBranch(stream, true));
+            }
+
+            HuffmanNode<T> root = GenerateNode(new BitStream());
+
+            if (totalLeaves != paths.Count){
+                throw new ArgumentException("Impossible symbol paths, " + (paths.Count - totalLeaves) + " symbol(s) could not be reached.", nameof(paths));
+            }
+
+            return root;
+        }
+    }
+}
---
+++ UnitTests/Huffman/TestHuffmanGenerator.fs
@@ -0,0 +1,99 @@
+﻿namespace UnitTests.Huffman.TestHuffmanGenerator
+
+open Xunit
+open System.Collections.Generic
+open BrotliLib.Huffman
+open BrotliLib.IO
+
+
+module FromBitCounts =
+    open System
+
+    [<Fact>]
+    let ``1 character with length (0) generates empty bit stream`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream(""))
+        }, HuffmanGenerator.GenerateFromBitCounts([|
+            HuffmanGenerator<_>.Entry('a', 0uy)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``2 characters with lengths (1, 1) generate correct bit stream`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("0"))
+            yield KeyValuePair('b', BitStream("1"))
+        }, HuffmanGenerator.GenerateFromBitCounts([|
+            HuffmanGenerator<_>.Entry('a', 1uy);
+            HuffmanGenerator<_>.Entry('b', 1uy)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``3 characters with lengths (2, 2, 1) generate correct bit stream`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('c', BitStream("0"))
+            yield KeyValuePair('a', BitStream("10"))
+            yield KeyValuePair('b', BitStream("11"))
+        }, HuffmanGenerator.GenerateFromBitCounts([|
+            HuffmanGenerator<_>.Entry('a', 2uy);
+            HuffmanGenerator<_>.Entry('b', 2uy);
+            HuffmanGenerator<_>.Entry('c', 1uy)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``4 characters with lengths (2, 2, 2, 2) generate correct bit stream`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("00"))
+            yield KeyValuePair('b', BitStream("01"))
+            yield KeyValuePair('c', BitStream("10"))
+            yield KeyValuePair('d', BitStream("11"))
+        }, HuffmanGenerator.GenerateFromBitCounts([|
+            HuffmanGenerator<_>.Entry('a', 2uy);
+            HuffmanGenerator<_>.Entry('b', 2uy);
+            HuffmanGenerator<_>.Entry('c', 2uy);
+            HuffmanGenerator<_>.Entry('d', 2uy)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``4 characters with lengths (2, 1, 3, 3) generate correct bit streams`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('b', BitStream("0"))
+            yield KeyValuePair('a', BitStream("10"))
+            yield KeyValuePair('c', BitStream("110"))
+            yield KeyValuePair('d', BitStream("111"))
+        }, HuffmanGenerator.GenerateFromBitCounts([|
+            HuffmanGenerator<_>.Entry('a', 2uy);
+            HuffmanGenerator<_>.Entry('b', 1uy);
+            HuffmanGenerator<_>.Entry('c', 3uy);
+            HuffmanGenerator<_>.Entry('d', 3uy)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``2 characters with lengths (1, 0) throw exception`` () =
+        let f () =
+            HuffmanGenerator.GenerateFromBitCounts([|
+                HuffmanGenerator<_>.Entry('a', 1uy);
+                HuffmanGenerator<_>.Entry('b', 0uy)
+            |])
+
+        Assert.Throws<ArgumentException>(fun () -> f() |> ignore)
+
+    [<Fact>]
+    let ``2 characters with lengths (1, 2) throw exception`` () =
+        let f () =
+            HuffmanGenerator.GenerateFromBitCounts([|
+                HuffmanGenerator<_>.Entry('a', 1uy);
+                HuffmanGenerator<_>.Entry('b', 2uy)
+            |])
+
+        Assert.Throws<ArgumentException>(fun () -> f() |> ignore)
+
+    [<Fact>]
+    let ``3 characters with lengths (1, 1, 1) throw exception`` () =
+        let f () =
+            HuffmanGenerator.GenerateFromBitCounts([|
+                HuffmanGenerator<_>.Entry('a', 1uy);
+                HuffmanGenerator<_>.Entry('b', 1uy);
+                HuffmanGenerator<_>.Entry('c', 1uy)
+            |])
+
+        Assert.Throws<ArgumentException>(fun () -> f() |> ignore)
