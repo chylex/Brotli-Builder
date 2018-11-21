6e3a50d9a636844e67d9a47a60b9048ab0620d6d
chylex <contact@chylex.com>
2018-11-21 18:02:49 +0100

    Add fixed-size RingBuffer w/ unit tests

---
+++ BrotliLib/Collections/RingBuffer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BrotliLib.Collections{
+    /// <summary>
+    /// A ring buffer implemented as a fixed-size queue.
+    /// </summary>
+    public sealed class RingBuffer<T>{
+        /// <summary>
+        /// Amount of elements in the buffer.
+        /// </summary>
+        public int Length => values.Length;
+
+        /// <summary>
+        /// Element at index [<see cref="Length"/> - 1].
+        /// </summary>
+        public T Front => this[values.Length - 1];
+
+        /// <summary>
+        /// Element at index [0].
+        /// </summary>
+        public T Back => this[0];
+
+        /// <summary>
+        /// Element at the provided <paramref name="index"/>.
+        /// </summary>
+        /// <exception cref="IndexOutOfRangeException">Thrown when the provided <paramref name="index"/> is negative.</exception>
+        public T this[int index]{
+            get{
+                if (index < 0){
+                    throw new IndexOutOfRangeException("Ring buffer index cannot be negative.");
+                }
+
+                return values[(index + accessOffset) % values.Length];
+            }
+        }
+
+        private readonly T[] values;
+        private int accessOffset;
+
+        /// <summary>
+        /// Initializes a new ring buffer, with the values and size of the provided <paramref name="values"/>.
+        /// </summary>
+        /// <param name="values">Initial values placed into the buffer. Element at [0] is at the back of the queue.</param>
+        public RingBuffer(params T[] values){
+            if (values.Length == 0){
+                throw new ArgumentException("Ring buffer size cannot be zero.");
+            }
+
+            this.values = values;
+        }
+
+        /// <summary>
+        /// Pushes a new value to the front of the queue, removing the value at the back.
+        /// </summary>
+        public void Push(T value){
+            values[accessOffset++ % values.Length] = value;
+        }
+    }
+}
---
+++ UnitTests/Collections/TestRingBuffer.fs
@@ -0,0 +1,95 @@
+﻿namespace UnitTests.Collections.TestRingBuffer
+
+open Xunit
+open System
+open BrotliLib.Collections
+
+
+module InitializationAndAccess =
+    [<Fact>]
+    let ``constructing with one value initializes the buffer correctly`` () =
+        let buffer = RingBuffer(10)
+
+        Assert.Equal(1, buffer.Length)
+        Assert.Equal(10, buffer.Front)
+        Assert.Equal(10, buffer.Back)
+
+        Assert.Equal(10, buffer.[0])
+        Assert.Equal(10, buffer.[1])
+        Assert.Equal(10, buffer.[2])
+
+    [<Fact>]
+    let ``constructing with two values initializes the buffer correctly`` () =
+        let buffer = RingBuffer(10, 20)
+
+        Assert.Equal(2, buffer.Length)
+        Assert.Equal(20, buffer.Front)
+        Assert.Equal(10, buffer.Back)
+
+        Assert.Equal(10, buffer.[0])
+        Assert.Equal(20, buffer.[1])
+        Assert.Equal(10, buffer.[2])
+
+    [<Fact>]
+    let ``constructing with three values initializes the buffer correctly`` () =
+        let buffer = RingBuffer(10, 20, 30)
+
+        Assert.Equal(3, buffer.Length)
+        Assert.Equal(30, buffer.Front)
+        Assert.Equal(10, buffer.Back)
+
+        Assert.Equal(10, buffer.[0])
+        Assert.Equal(20, buffer.[1])
+        Assert.Equal(30, buffer.[2])
+
+    [<Fact>]
+    let ``constructing with no parameters throws exception`` () =
+        Assert.Throws<ArgumentException>(fun () -> RingBuffer() |> ignore)
+
+    [<Fact>]
+    let ``accessing a negative index throws exception`` () =
+        let buffer = RingBuffer(10, 20, 30)
+        Assert.Throws<IndexOutOfRangeException>(fun () -> buffer.[-1] |> ignore)
+
+
+module Mutability =
+    [<Fact>]
+    let ``pushing to a buffer of size 1 replaces the stored value`` () =
+        let buffer = RingBuffer(10)
+
+        buffer.Push(90)
+        Assert.Equal(90, buffer.Front)
+
+    [<Fact>]
+    let ``pushing to a buffer of size 2 inserts the value and shifts the rest`` () =
+        let buffer = RingBuffer(10, 20)
+
+        buffer.Push(90)
+        Assert.Equal(90, buffer.Front)
+        Assert.Equal(20, buffer.Back)
+
+        buffer.Push(80)
+        Assert.Equal(80, buffer.Front)
+        Assert.Equal(90, buffer.Back)
+
+    [<Fact>]
+    let ``pushing to a buffer of size 3 inserts the value and shifts the rest`` () =
+        let buffer = RingBuffer(10, 20, 30)
+
+        buffer.Push(90)
+        Assert.Equal(90, buffer.Front)
+        Assert.Equal(30, buffer.[1])
+        Assert.Equal(20, buffer.Back)
+
+        buffer.Push(80)
+        Assert.Equal(80, buffer.Front)
+        Assert.Equal(90, buffer.[1])
+        Assert.Equal(30, buffer.Back)
+
+    [<Fact>]
+    let ``accessing a negative index after pushing throws exception`` () =
+        let buffer = RingBuffer(10, 20, 30)
+
+        buffer.Push(90)
+        buffer.Push(80)
+        Assert.Throws<IndexOutOfRangeException>(fun () -> buffer.[-1] |> ignore)
