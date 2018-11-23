286f476cfbeabe6c7e131f04a7695728a35c56fb
chylex <contact@chylex.com>
2018-11-23 07:45:01 +0100

    Add AlphabetSize utility w/ unit tests

---
+++ BrotliLib/Brotli/Components/Utils/AlphabetSize.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BrotliLib.Brotli.Components.Utils{
+    /// <summary>
+    /// Defines properties of a symbol alphabet.
+    /// </summary>
+    public struct AlphabetSize{
+        /// <summary>
+        /// Returns the minimum amount of bits required to represent every symbol in the alphabet.
+        /// </summary>
+        public int BitsPerSymbol{
+            get{
+                int size = Value - 1;
+                int bitsPerSymbol = 0;
+            
+                while(size > 0){
+                    size >>= 1;
+                    ++bitsPerSymbol;
+                }
+
+                return bitsPerSymbol;
+            }
+        }
+
+        /// <summary>
+        /// Amount of symbols in the alphabet.
+        /// </summary>
+        public int Value { get; }
+        
+        public AlphabetSize(int value){
+            if (value < 0){
+                throw new ArgumentOutOfRangeException(nameof(value), "Alphabet cannot have a negative amount of symbols.");
+            }
+
+            this.Value = value;
+        }
+    }
+}
---
+++ UnitTests/Brotli/TestUtils.fs
@@ -0,0 +1,31 @@
+﻿namespace UnitTests.Brotli.TestUtils
+
+open Xunit
+open System
+open BrotliLib.Brotli.Components.Utils
+
+
+module AlphabetSize =
+    [<Theory>]
+    [<InlineData(0, 0)>]
+    [<InlineData(1, 0)>]
+    [<InlineData(2, 1)>]
+    [<InlineData(3, 2)>]
+    [<InlineData(4, 2)>]
+    [<InlineData(5, 3)>]
+    [<InlineData(6, 3)>]
+    [<InlineData(7, 3)>]
+    [<InlineData(8, 3)>]
+    [<InlineData(9, 4)>]
+    [<InlineData(16, 4)>]
+    [<InlineData(17, 5)>]
+    [<InlineData(32, 5)>]
+    [<InlineData(33, 6)>]
+    [<InlineData(256, 8)>]
+    [<InlineData(704, 10)>]
+    let ``alphabet of specified size calculates amount of bits per symbol correctly`` (symbols: int, bits: int) =
+        Assert.Equal(bits, AlphabetSize(symbols).BitsPerSymbol)
+
+    [<Fact>]
+    let ``constructing alphabet with negative amount of symbols throws exception`` () =
+        Assert.Throws<ArgumentOutOfRangeException>(fun () -> AlphabetSize(-1) |> ignore)
