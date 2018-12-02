ed5e7cd07dd4e508506462829858fe270ff0a0fe
chylex <contact@chylex.com>
2018-12-02 16:00:57 +0100

    Visual Studio is acting up

---
+++ BrotliBuilder/Utils/ControlExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Windows.Forms;
+
+namespace BrotliBuilder.Utils{
+    static class ControlExtensions{
+        public static void EnableDoubleBuffering(this DataGridView dataGridView){
+            // Visual Studio is garbage and the designer crashes if DataGridView is extended, so reflection it is
+            dataGridView.GetType().GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic)?.SetValue(dataGridView, true, null);
+        }
+    }
+}
