dc63c1355dee22f40341664acbf04f30b6205140
chylex <contact@chylex.com>
2018-12-02 14:13:22 +0100

    Implement file and memory representations of a Brotli dictionary

---
+++ BrotliLib/Brotli/Dictionary/Source/MemorySource.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace BrotliLib.Brotli.Dictionary.Source{
+    /// <summary>
+    /// Reads dictionary words from bytes stored in memory.
+    /// </summary>
+    public sealed class MemorySource : IDictionarySource{
+        private readonly MemoryStream stream;
+        
+        /// <summary>
+        /// Initializes the source from a file, which is read once and stored in memory.
+        /// </summary>
+        public MemorySource(string filePath){
+            this.stream = new MemoryStream(File.ReadAllBytes(filePath));
+        }
+        
+        public void Dispose(){
+            stream.Dispose();
+        }
+
+        byte[] IDictionarySource.ReadBytes(int position, int count){
+            byte[] bytes = new byte[count];
+            stream.Position = position;
+            stream.Read(bytes, 0, count);
+            return bytes;
+        }
+    }
+}
