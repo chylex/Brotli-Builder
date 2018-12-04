8610d6d1cff60c5115218395216a032d2b8d924d
chylex <contact@chylex.com>
2018-12-04 17:21:51 +0100

    Add IDictionarySource implementation that can take any Stream

--- BrotliLib/Brotli/Dictionary/Source/OpenFileSource.cs
+++ BrotliLib/Brotli/Dictionary/Source/OpenFileSource.cs
@@ -4,22 +4,7 @@ namespace BrotliLib.Brotli.Dictionary.Source{
     /// <summary>
     /// Reads dictionary words from a file, which will stay open and block write access until disposed.
     /// </summary>
-    public sealed class OpenFileSource : IDictionarySource{
-        private readonly FileStream file;
-        
-        public OpenFileSource(string filePath){
-            this.file = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-        }
-        
-        public void Dispose(){
-            file.Dispose();
-        }
-
-        byte[] IDictionarySource.ReadBytes(int position, int count){
-            byte[] bytes = new byte[count];
-            file.Seek(position, SeekOrigin.Begin);
-            file.Read(bytes, 0, count);
-            return bytes;
-        }
+    public sealed class OpenFileSource : StreamSource{
+        public OpenFileSource(string filePath) : base(new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read)){}
     }
 }
---
+++ BrotliLib/Brotli/Dictionary/Source/StreamSource.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace BrotliLib.Brotli.Dictionary.Source{
+    /// <summary>
+    /// Reads dictionary words from a generic stream.
+    /// </summary>
+    public class StreamSource : IDictionarySource{
+        private readonly Stream stream;
+
+        public StreamSource(Stream stream){
+            this.stream = stream;
+        }
+
+        public void Dispose(){
+            stream.Dispose();
+        }
+
+        byte[] IDictionarySource.ReadBytes(int position, int count){
+            byte[] bytes = new byte[count];
+            stream.Seek(position, SeekOrigin.Begin);
+            stream.Read(bytes, 0, count);
+            return bytes;
+        }
+    }
+}
