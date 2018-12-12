a3e4983bc51f04e22321036cfa61ffbe987adaa2
chylex <contact@chylex.com>
2018-12-12 23:05:23 +0100

    Implement Move-to-front & inverse transformation w/ unit tests

---
+++ BrotliLib/Brotli/Components/Utils/MoveToFront.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace BrotliLib.Brotli.Components.Utils{
+    public static class MoveToFront{
+        /// <summary>
+        /// Performs an in-place move-to-front transformation on the input byte array.
+        /// </summary>
+        public static readonly Action<byte[]> Encode = contextMap => {
+            byte[] mtf = CreateAlphabet(contextMap.Max() + 1);
+
+            for(int index = 0; index < contextMap.Length; index++){
+                byte mtfIndex = (byte)Array.IndexOf(mtf, contextMap[index]);
+                byte value = mtf[mtfIndex];
+
+                contextMap[index] = mtfIndex;
+                MoveValueToFront(mtf, mtfIndex, value);
+            }
+        };
+        
+        /// <summary>
+        /// Performs an in-place inverse-move-to-front transformation on the input byte array.
+        /// </summary>
+        public static readonly Action<byte[]> Decode = contextMap => {
+            byte[] mtf = CreateAlphabet(256);
+
+            for(int index = 0; index < contextMap.Length; index++){
+                byte mapIndex = contextMap[index];
+                byte value = mtf[mapIndex];
+
+                contextMap[index] = value;
+                MoveValueToFront(mtf, mapIndex, value);
+            }
+        };
+
+        // Helpers
+
+        private static byte[] CreateAlphabet(int size){
+            byte[] mtf = new byte[size];
+
+            for(int index = 0; index < mtf.Length; index++){
+                mtf[index] = (byte)index;
+            }
+
+            return mtf;
+        }
+
+        private static void MoveValueToFront(byte[] mtf, byte index, byte value){
+            while(index > 0){
+                mtf[index] = mtf[--index];
+            }
+
+            mtf[0] = value;
+        }
+    }
+}
--- UnitTests/Brotli/TestUtils.fs
+++ UnitTests/Brotli/TestUtils.fs
@@ -2,6 +2,7 @@
 
 open Xunit
 open System
+open System.Collections.Generic
 open BrotliLib.Brotli.Components.Utils
 
 
@@ -29,3 +30,38 @@ module AlphabetSize =
     [<Fact>]
     let ``constructing alphabet with negative amount of symbols throws exception`` () =
         Assert.Throws<ArgumentOutOfRangeException>(fun () -> AlphabetSize(-1) |> ignore)
+
+
+module MoveToFront =
+    let maps : obj array seq = seq {
+        yield Array.map box [|   0uy;  0uy;   0uy;  0uy;   0uy;  0uy; (**)   0uy; 0uy; 0uy;  0uy; 0uy; 0uy |]
+        yield Array.map box [|   0uy;  1uy;   2uy;  0uy;   1uy;  2uy; (**)   0uy; 1uy; 2uy;  2uy; 2uy; 2uy |]
+        yield Array.map box [|   5uy;  4uy;   4uy;  3uy;   1uy;  0uy; (**)   5uy; 5uy; 0uy;  5uy; 4uy; 4uy |]
+        yield Array.map box [|  10uy; 10uy;  10uy; 20uy;  20uy; 20uy; (**)  10uy; 0uy; 0uy; 20uy; 0uy; 0uy |]
+        yield Array.map box [| 255uy;  0uy; 255uy;  0uy; 255uy;  0uy; (**) 255uy; 1uy; 1uy;  1uy; 1uy; 1uy |]
+    }
+
+    [<Theory>]
+    [<MemberData("maps")>]
+    let ``encode produces expected result`` ([<ParamArray>] elements: byte array) =
+        let map = elements.[..(elements.Length / 2 - 1)]
+        MoveToFront.Encode.Invoke(map)
+
+        Assert.Equal<IEnumerable<byte>>(elements.[(elements.Length / 2)..], map)
+
+    [<Theory>]
+    [<MemberData("maps")>]
+    let ``decode produces expected result`` ([<ParamArray>] elements: byte array) =
+        let map = elements.[(elements.Length / 2)..]
+        MoveToFront.Decode.Invoke(map)
+
+        Assert.Equal<IEnumerable<byte>>(elements.[..(elements.Length / 2 - 1)], map)
+
+    [<Theory>]
+    [<MemberData("maps")>]
+    let ``encode followed by decode produces same values`` ([<ParamArray>] elements: byte array) =
+        let map = elements.[..(elements.Length / 2 - 1)]
+        MoveToFront.Encode.Invoke(map)
+        MoveToFront.Decode.Invoke(map)
+
+        Assert.Equal<IEnumerable<byte>>(elements.[..(elements.Length / 2 - 1)], map)
