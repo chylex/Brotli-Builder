0add08098e2cd1cc6d86ca13f65fe6eca1de599e
chylex <contact@chylex.com>
2018-12-14 21:46:03 +0100

    Implement limited-depth Huffman tree generation from frequency map w/ unit tests

--- BrotliLib/Huffman/HuffmanGenerator.cs
+++ BrotliLib/Huffman/HuffmanGenerator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using BrotliLib.Collections;
 using BrotliLib.IO;
 
 namespace BrotliLib.Huffman{
@@ -18,9 +19,15 @@ namespace BrotliLib.Huffman{
             public T Symbol { get; }
             public byte Bits { get; }
 
+            internal double Kraft => Math.Pow(2, -Bits);
+
             public Entry(T symbol, byte bits){
                 Symbol = symbol;
                 Bits = bits;
+            }
+
+            internal Entry Resize(byte bits){
+                return new Entry(Symbol, bits);
             }
 
             public int CompareTo(Entry other){
@@ -112,5 +119,140 @@ namespace BrotliLib.Huffman{
 
             return root;
         }
+
+        #endregion
+        
+        #region Frequencies
+
+        /// <summary>
+        /// Pairs a <see cref="HuffmanNode{T}"/> with its frequency, used while constructing a tree.
+        /// </summary>
+        public class Freq : IComparable<Freq>{
+            public HuffmanNode<T> Node { get; }
+            public int Frequency { get; }
+
+            public Freq(HuffmanNode<T> node, int frequency){
+                Node = node;
+                Frequency = frequency;
+            }
+            
+            public int CompareTo(Freq other){
+                return Frequency.CompareTo(other.Frequency);
+            }
+
+            public void Deconstruct(out HuffmanNode<T> node, out int frequency){
+                node = Node;
+                frequency = Frequency;
+            }
+        }
+
+        /// <summary>
+        /// Pairs a symbol with its frequency, used while constructing a tree.
+        /// </summary>
+        public sealed class SymbolFreq : Freq{
+            public T Symbol { get; }
+
+            public SymbolFreq(T symbol, int frequency) : base(new HuffmanNode<T>.Leaf(symbol), frequency){
+                this.Symbol = symbol;
+            }
+        }
+
+        public static SymbolFreq MakeFreq<U>(IGrouping<T, U> grouping){
+            return new SymbolFreq(grouping.Key, grouping.Count());
+        }
+
+        /// <summary>
+        /// Generates a Huffman tree from a list of symbols with their frequencies. There must be at least 1 symbol in the <paramref name="symbols"/> list.
+        /// If the list contains a single node with any frequency, that node will be returned.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="symbols"/> list is empty.</exception>
+        public static HuffmanNode<T> FromFrequencies(IList<SymbolFreq> symbols){
+            if (symbols.Count == 1){
+                return symbols[0].Node;
+            }
+            else if (symbols.Count == 0){
+                throw new ArgumentException("Cannot generate a Huffman tree with no symbols.", nameof(symbols));
+            }
+
+            var nodes = new PriorityQueue<Freq>();
+
+            foreach(SymbolFreq entry in symbols){
+                nodes.Insert(entry);
+            }
+            
+            while(nodes.Count > 1){
+                var (node1, freq1) = nodes.ExtractMin();
+                var (node2, freq2) = nodes.ExtractMin();
+
+                var newPath = freq1 <= freq2 ? new HuffmanNode<T>.Path(node1, node2) : new HuffmanNode<T>.Path(node2, node1);
+                nodes.Insert(new Freq(newPath, freq1 + freq2));
+            }
+
+            return nodes.ExtractMin().Node;
+        }
+
+        /// <summary>
+        /// Generates a canonical Huffman tree from a list of symbols with their frequencies. There must be at least 1 symbol in the <paramref name="symbols"/> list.
+        /// If the list contains a single node with any frequency, that node will be returned.
+        /// <para/>
+        /// The additional <paramref name="maxDepth"/> parameter will limit the maximum length of paths in the tree.
+        /// Depth limiting is implemented using a heuristic described at https://cbloomrants.blogspot.com/2010/07/07-03-10-length-limitted-huffman-codes.html.
+        /// </summary>
+        /// <exception cref="ArgumentException">Thrown when the <paramref name="symbols"/> list is empty.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when <paramref name="maxDepth"/> is smaller than 1 or larger than 31.</exception>
+        public static HuffmanNode<T> FromFrequenciesCanonical(IList<SymbolFreq> symbols, byte maxDepth){
+            if (maxDepth < 1 || maxDepth > 31){
+                throw new ArgumentOutOfRangeException(nameof(maxDepth), "Depth-limited trees must have a specified maximum depth between 1 and 31.");
+            }
+
+            if (symbols.Count > (1 << maxDepth)){
+                throw new ArgumentException("Maximum depth " + maxDepth + " can only encode up to " + (1 << maxDepth) + " symbol(s), amount of provided symbols is " + symbols.Count + ".", nameof(maxDepth));
+            }
+            
+            var lengthMap = FromFrequencies(symbols).GenerateValueMap();
+
+            if (lengthMap.All(entry => entry.Value.Length <= maxDepth)){
+                return FromBitCountsCanonical(lengthMap.Select(kvp => new Entry(kvp.Key, (byte)kvp.Value.Length)).ToArray());
+            }
+
+            Entry MakeLimitedDepthEntry(T symbol){
+                return new Entry(symbol, Math.Min((byte)lengthMap[symbol].Length, maxDepth));
+            }
+            
+            var symbolOrder = symbols.Select(entry => entry.Symbol).ToArray();
+            
+            var symbolEntries = symbols.OrderBy(entry => entry.Frequency)
+                                       .ThenBy(entry => Array.IndexOf(symbolOrder, entry.Symbol))
+                                       .Select(entry => MakeLimitedDepthEntry(entry.Symbol))
+                                       .ToArray();
+            
+            double bitSpace = symbolEntries.Sum(entry => entry.Kraft);
+
+            for(int index = 0; index < symbolEntries.Length && bitSpace > 1; index++){
+                var entry = symbolEntries[index];
+
+                if (entry.Bits < maxDepth){
+                    entry = entry.Resize((byte)(entry.Bits + 1));
+
+                    symbolEntries[index] = entry;
+                    bitSpace -= entry.Kraft;
+                }
+            }
+
+            for(int index = symbolEntries.Length - 1; index > 0 && bitSpace < 1; index--){
+                var entry = symbolEntries[index];
+
+                if (bitSpace + entry.Kraft <= 1){
+                    entry = entry.Resize((byte)(entry.Bits - 1));
+
+                    symbolEntries[index] = entry;
+                    bitSpace += entry.Kraft;
+                }
+            }
+
+            return FromBitCountsCanonical(symbolEntries);
+        }
+
+        #endregion
     }
 }
--- UnitTests/Huffman/TestHuffmanGenerator.fs
+++ UnitTests/Huffman/TestHuffmanGenerator.fs
@@ -124,3 +124,145 @@ module FromBitCountsCanonical =
             |])
 
         Assert.Throws<ArgumentException>(fun () -> f() |> ignore)
+        
+
+module FromFrequencies =
+    [<Fact>]
+    let ``1 character returns itself`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream())
+        }, HuffmanGenerator.FromFrequencies([|
+            HuffmanGenerator<_>.SymbolFreq('a', 5)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``2 characters return correct bit streams`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("0"))
+            yield KeyValuePair('b', BitStream("1"))
+        }, HuffmanGenerator.FromFrequencies([|
+            HuffmanGenerator<_>.SymbolFreq('b', 2);
+            HuffmanGenerator<_>.SymbolFreq('a', 1)
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``3 characters return correct bit streams`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("00"))
+            yield KeyValuePair('b', BitStream("01"))
+            yield KeyValuePair('c', BitStream("1"))
+        }, HuffmanGenerator.FromFrequencies([|
+            HuffmanGenerator<_>.SymbolFreq('b', 2);
+            HuffmanGenerator<_>.SymbolFreq('a', 1);
+            HuffmanGenerator<_>.SymbolFreq('c', 5);
+        |]).GenerateValueMap())
+
+    [<Fact>]
+    let ``many characters return correct bit path lengths`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair(' ', 3)
+            yield KeyValuePair('a', 3)
+            yield KeyValuePair('e', 3)
+            yield KeyValuePair('f', 4)
+            yield KeyValuePair('h', 4)
+            yield KeyValuePair('i', 4)
+            yield KeyValuePair('m', 4)
+            yield KeyValuePair('n', 4)
+            yield KeyValuePair('s', 4)
+            yield KeyValuePair('t', 4)
+            yield KeyValuePair('l', 5)
+            yield KeyValuePair('o', 5)
+            yield KeyValuePair('p', 5)
+            yield KeyValuePair('r', 5)
+            yield KeyValuePair('u', 5)
+            yield KeyValuePair('x', 5)
+        }, HuffmanGenerator.FromFrequencies(
+            "this is an example of a huffman tree".ToCharArray()
+            |> Seq.countBy(fun x -> x)
+            |> Seq.map(fun (char, count) -> HuffmanGenerator<_>.SymbolFreq(char, count))
+            |> Seq.toArray
+        ).GenerateValueMap() |> Seq.map(fun (kvp) -> KeyValuePair(kvp.Key, kvp.Value.Length)) |> Seq.sortBy(fun (kvp) -> (kvp.Value, kvp.Key)))
+
+    [<Fact>]
+    let ``0 characters throw exception`` () =
+        Assert.Throws<ArgumentException>(fun () -> HuffmanGenerator<char>.FromFrequencies([||]) |> ignore)
+
+
+module FromFrequenciesCanonical =
+    [<Fact>]
+    let ``limiting worst case depth 6 tree to depth 6 results in no change`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("0"))
+            yield KeyValuePair('b', BitStream("10"))
+            yield KeyValuePair('c', BitStream("110"))
+            yield KeyValuePair('d', BitStream("1110"))
+            yield KeyValuePair('e', BitStream("11110"))
+            yield KeyValuePair('f', BitStream("111110"))
+            yield KeyValuePair('g', BitStream("111111"))
+        }, HuffmanGenerator.FromFrequenciesCanonical([|
+            HuffmanGenerator<_>.SymbolFreq('a', 13)
+            HuffmanGenerator<_>.SymbolFreq('b', 8)
+            HuffmanGenerator<_>.SymbolFreq('c', 5)
+            HuffmanGenerator<_>.SymbolFreq('d', 3)
+            HuffmanGenerator<_>.SymbolFreq('e', 2)
+            HuffmanGenerator<_>.SymbolFreq('f', 1)
+            HuffmanGenerator<_>.SymbolFreq('g', 1)
+        |], 6uy).GenerateValueMap())
+
+    [<Fact>]
+    let ``limiting worst case depth 6 tree to depth 5 generates correct bit paths`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("0"))
+            yield KeyValuePair('b', BitStream("10"))
+            yield KeyValuePair('c', BitStream("110"))
+            yield KeyValuePair('d', BitStream("11100"))
+            yield KeyValuePair('e', BitStream("11101"))
+            yield KeyValuePair('f', BitStream("11110"))
+            yield KeyValuePair('g', BitStream("11111"))
+        }, HuffmanGenerator.FromFrequenciesCanonical([|
+            HuffmanGenerator<_>.SymbolFreq('a', 13)
+            HuffmanGenerator<_>.SymbolFreq('b', 8)
+            HuffmanGenerator<_>.SymbolFreq('c', 5)
+            HuffmanGenerator<_>.SymbolFreq('d', 3)
+            HuffmanGenerator<_>.SymbolFreq('e', 2)
+            HuffmanGenerator<_>.SymbolFreq('f', 1)
+            HuffmanGenerator<_>.SymbolFreq('g', 1)
+        |], 5uy).GenerateValueMap())
+
+    [<Fact>]
+    let ``limiting worst case depth 6 tree to depth 4 generates correct bit paths`` () =
+        Assert.Equal<IEnumerable<KeyValuePair<_, _>>>(seq {
+            yield KeyValuePair('a', BitStream("0"))
+            yield KeyValuePair('b', BitStream("100"))
+            yield KeyValuePair('c', BitStream("101"))
+            yield KeyValuePair('d', BitStream("1100"))
+            yield KeyValuePair('e', BitStream("1101"))
+            yield KeyValuePair('f', BitStream("1110"))
+            yield KeyValuePair('g', BitStream("1111"))
+        }, HuffmanGenerator.FromFrequenciesCanonical([|
+            HuffmanGenerator<_>.SymbolFreq('a', 13)
+            HuffmanGenerator<_>.SymbolFreq('b', 8)
+            HuffmanGenerator<_>.SymbolFreq('c', 5)
+            HuffmanGenerator<_>.SymbolFreq('d', 3)
+            HuffmanGenerator<_>.SymbolFreq('e', 2)
+            HuffmanGenerator<_>.SymbolFreq('f', 1)
+            HuffmanGenerator<_>.SymbolFreq('g', 1)
+        |], 4uy).GenerateValueMap())
+
+    [<Theory>]
+    [<InlineData(0uy)>]
+    [<InlineData(32uy)>]
+    let ``invalid maximum depths throw exception`` (maxDepth: byte) =
+        Assert.Throws<ArgumentOutOfRangeException>(fun () -> HuffmanGenerator<char>.FromFrequenciesCanonical([||], maxDepth) |> ignore)
+
+    [<Fact>]
+    let ``limiting tree with 5 symbols to depth 2 throws exception`` () =
+        let freq = [|
+            HuffmanGenerator<_>.SymbolFreq('a', 5)
+            HuffmanGenerator<_>.SymbolFreq('b', 3)
+            HuffmanGenerator<_>.SymbolFreq('c', 2)
+            HuffmanGenerator<_>.SymbolFreq('d', 1)
+            HuffmanGenerator<_>.SymbolFreq('e', 1)
+        |]
+
+        Assert.Throws<ArgumentException>(fun () -> HuffmanGenerator.FromFrequenciesCanonical(freq, 2uy) |> ignore)
