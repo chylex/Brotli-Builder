a7b6d7047d7daddad095cc168e15e399648d3efe
chylex <contact@chylex.com>
2019-02-08 09:24:51 +0100

    Implement Brotli block type and block length code serialization

---
+++ BrotliLib/Brotli/Components/Data/BlockLengthCode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using BrotliLib.Brotli.Components.Header;
+using BrotliLib.Brotli.Components.Utils;
+using BrotliLib.IO;
+using BlockLengthCodeTree = BrotliLib.Brotli.Components.Header.HuffmanTree<BrotliLib.Brotli.Components.Data.BlockLengthCode>;
+
+namespace BrotliLib.Brotli.Components.Data{
+    /// <summary>
+    /// Describes a <see cref="HuffmanTree{T}"/> entry used to calculate the length of a block in a block switch command.
+    /// https://tools.ietf.org/html/rfc7932#section-6
+    /// </summary>
+    public sealed class BlockLengthCode : IComparable<BlockLengthCode>{
+        public static readonly AlphabetSize AlphabetSize = new AlphabetSize(26);
+        public static readonly BlockLengthCodeTree.Context TreeContext = new BlockLengthCodeTree.Context(AlphabetSize, value => new BlockLengthCode(value), symbol => symbol.Code);
+
+        // Block length tables
+
+        private static readonly int[] BlockLengthExtraBits = {
+            2, 2, 2,  2,  3,  3,  3,  3, 4,
+            4, 4, 4,  5,  5,  5,  5,  6, 6,
+            7, 8, 9, 10, 11, 12, 13, 24,
+        };
+
+        private static readonly int[] BlockLengthOffsets = {
+              1,   5,   9,   13,   17,   25,   33,    41,  49,
+             65,  81,  97,  113,  145,  177,  209,   241, 305,
+            369, 497, 753, 1265, 2289, 4337, 8433, 16625,
+        };
+
+        // Data
+
+        public int Code { get; }
+
+        public BlockLengthCode(int code){
+            this.Code = code;
+        }
+
+        public bool CanEncodeValue(int value){
+            int valueNormalized = value - BlockLengthOffsets[Code];
+            return valueNormalized >= 0 && valueNormalized < (1 << BlockLengthExtraBits[Code]);
+        }
+
+        private int ReadValue(BitReader reader){
+            return BlockLengthOffsets[Code] + reader.NextChunk(BlockLengthExtraBits[Code]);
+        }
+
+        private void WriteValue(BitWriter writer, int value){
+            writer.WriteChunk(BlockLengthExtraBits[Code], value - BlockLengthOffsets[Code]);
+        }
+
+        public int CompareTo(BlockLengthCode other){
+            return Code.CompareTo(other.Code);
+        }
+
+        public override int GetHashCode(){
+            return Code;
+        }
+
+        public override bool Equals(object obj){
+            return obj is BlockLengthCode other && other.Code == Code;
+        }
+
+        public override string ToString(){
+            return "{ Code = " + Code + " }";
+        }
+
+        // Serialization
+
+        internal static readonly IBitSerializer<int, BlockLengthCode> Serializer = new BitSerializer<int, BlockLengthCode>(
+            fromBits: (reader, context) => context.ReadValue(reader),
+            toBits: (writer, obj, context) => context.WriteValue(writer, obj)
+        ); 
+    }
+}
---
+++ BrotliLib/Brotli/Components/Header/BlockTypeInfo.cs
@@ -0,0 +1,96 @@
+﻿using BrotliLib.Brotli.Components.Utils;
+using BrotliLib.IO;
+using System.Collections.Generic;
+using BrotliLib.Brotli.Components.Data;
+using BlockTypeCodeTree = BrotliLib.Brotli.Components.Header.HuffmanTree<int>;
+using BlockLengthCodeTree = BrotliLib.Brotli.Components.Header.HuffmanTree<BrotliLib.Brotli.Components.Data.BlockLengthCode>;
+
+namespace BrotliLib.Brotli.Components.Header{
+    /// <summary>
+    /// Describes information about block types of a <see cref="Category"/> within a meta-block.
+    /// https://tools.ietf.org/html/rfc7932#section-6
+    /// </summary>
+    public sealed class BlockTypeInfo{
+        public static readonly BlockTypeInfo Empty = new BlockTypeInfo();
+        
+        // Data
+
+        public int Count { get; }
+        public int InitialLength { get; }
+
+        public BlockTypeCodeTree TypeCodeTree { get; }
+        public BlockLengthCodeTree LengthCodeTree { get; }
+
+        private BlockTypeInfo(){
+            this.Count = 1;
+            this.InitialLength = 16777216;
+        }
+
+        public BlockTypeInfo(int count, int initialLength, BlockTypeCodeTree typeCodeTree, BlockLengthCodeTree lengthCodeTree){
+            this.Count = count;
+            this.InitialLength = initialLength;
+            this.TypeCodeTree = typeCodeTree;
+            this.LengthCodeTree = lengthCodeTree;
+        }
+        
+        public override int GetHashCode(){
+            unchecked{
+                var hashCode = -1945866543;
+                hashCode = hashCode * -1521134295 + Count.GetHashCode();
+                hashCode = hashCode * -1521134295 + InitialLength.GetHashCode();
+                hashCode = hashCode * -1521134295 + EqualityComparer<BlockTypeCodeTree>.Default.GetHashCode(TypeCodeTree);
+                hashCode = hashCode * -1521134295 + EqualityComparer<BlockLengthCodeTree>.Default.GetHashCode(LengthCodeTree);
+                return hashCode;
+            }
+        }
+
+        public override bool Equals(object obj){
+            return obj is BlockTypeInfo other &&
+                   Count == other.Count &&
+                   InitialLength == other.InitialLength &&
+                   EqualityComparer<BlockTypeCodeTree>.Default.Equals(TypeCodeTree, other.TypeCodeTree) &&
+                   EqualityComparer<BlockLengthCodeTree>.Default.Equals(LengthCodeTree, other.LengthCodeTree);
+        }
+
+        // Serialization
+
+        private static BlockTypeCodeTree.Context GetBlockTypeCodeTreeContext(int count){
+            return new BlockTypeCodeTree.Context(new AlphabetSize(count + 2), value => value, symbol => symbol);
+        }
+
+        public static readonly IBitSerializer<BlockTypeInfo, NoContext> Serializer = new BitSerializer<BlockTypeInfo, NoContext>(
+            fromBits: (reader, context) => {
+                int count = VariableLength11Code.Serializer.FromBits(reader, NoContext.Value).Value;
+
+                if (count == 1){
+                    return Empty;
+                }
+                
+                var blockTypeCode = BlockTypeCodeTree.Serializer.FromBits(reader, GetBlockTypeCodeTreeContext(count));
+                var blockLengthCode = BlockLengthCodeTree.Serializer.FromBits(reader, BlockLengthCode.TreeContext);
+
+                var initialLengthCode = blockLengthCode.Root.LookupValue(reader);
+                var initialLength = BlockLengthCode.Serializer.FromBits(reader, initialLengthCode);
+
+                return new BlockTypeInfo(count, initialLength, blockTypeCode, blockLengthCode);
+            },
+
+            toBits: (writer, obj, context) => {
+                VariableLength11Code.Serializer.ToBits(writer, new VariableLength11Code(obj.Count), NoContext.Value);
+
+                if (obj.Count == 1){
+                    return;
+                }
+
+                BlockTypeCodeTree.Serializer.ToBits(writer, obj.TypeCodeTree, GetBlockTypeCodeTreeContext(obj.Count));
+                BlockLengthCodeTree.Serializer.ToBits(writer, obj.LengthCodeTree, BlockLengthCode.TreeContext);
+
+                var initialLength = obj.InitialLength;
+                var initialLengthCode = obj.LengthCodeTree.FindEntry(code => code.CanEncodeValue(initialLength));
+
+                writer.WriteBits(initialLengthCode.Value);
+                BlockLengthCode.Serializer.ToBits(writer, initialLength, initialLengthCode.Key);
+            }
+        );
+    }
+}
