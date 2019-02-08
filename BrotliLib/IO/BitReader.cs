577786223ea52cea7c755d891308af2a461a573b
chylex <contact@chylex.com>
2019-02-08 10:46:22 +0100

    Allow BitReader to be wrapped and extended

--- BrotliLib/IO/BitReader.cs
+++ BrotliLib/IO/BitReader.cs
@@ -9,8 +9,9 @@ namespace BrotliLib.IO{
         private const int ByteSize = 8;
         private const int MaxChunkSize = ByteSize * sizeof(int);
 
+        protected virtual long Index { get; private set; }
+        
         private readonly IEnumerator<bool> enumerator;
-        private long index = 0;
 
         /// <summary>
         /// Initializes a new <see cref="BitReader"/> at the beginning of the specified <see cref="BitStream"/>.
@@ -18,19 +19,26 @@ namespace BrotliLib.IO{
         /// <param name="stream">Input bit stream.</param>
         public BitReader(BitStream stream){
             this.enumerator = stream.GetEnumerator();
-            this.index = 0;
+            this.Index = 0;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="BitReader"/> with no <see cref="BitStream"/>. Intended for use in <see cref="Wrapped"/> only.
+        /// </summary>
+        protected BitReader(){
+            this.enumerator = null;
         }
 
         /// <summary>
         /// Returns the next bit as a boolean.
         /// </summary>
         /// <exception cref="IndexOutOfRangeException">Thrown when there are no more bits left in the stream.</exception>
-        public bool NextBit(){
+        public virtual bool NextBit(){
             if (!enumerator.MoveNext()){
                 throw new IndexOutOfRangeException("No more bits left in the stream.");
             }
             
-            ++index;
+            ++Index;
             return enumerator.Current;
         }
 
@@ -41,7 +49,7 @@ namespace BrotliLib.IO{
         /// <param name="bits">Amount of bits to read.</param>
         /// <exception cref="IndexOutOfRangeException">Thrown when the <paramref name="bits"/> parameter is larger than the amount of bits left in the stream.</exception>
         /// <exception cref="ArgumentOutOfRangeException">Thrown when the <paramref name="bits"/> parameter is larger than the amount of bits in an integer.</exception>
-        public int NextChunk(int bits){
+        public virtual int NextChunk(int bits){
             if (bits > MaxChunkSize){
                 throw new ArgumentOutOfRangeException(nameof(bits), "Cannot retrieve a chunk larger than " + MaxChunkSize + " bits in one call.");
             }
@@ -61,8 +69,8 @@ namespace BrotliLib.IO{
         /// If the current position is not already at a byte boundary (position divisible by 8), enough bits will be skipped to align it.
         /// </summary>
         /// <exception cref="IndexOutOfRangeException">Thrown when there are not enough bits left in the stream for the alignment.</exception>
-        public void AlignToByteBoundary(){
-            long relativeIndex = index % ByteSize;
+        public virtual void AlignToByteBoundary(){
+            long relativeIndex = Index % ByteSize;
 
             if (relativeIndex > 0){
                 for(long bitsLeft = ByteSize - relativeIndex; bitsLeft > 0; bitsLeft--){
@@ -75,9 +83,39 @@ namespace BrotliLib.IO{
         /// First aligns the position to a byte boundary, then reads the following byte.
         /// </summary>
         /// <exception cref="IndexOutOfRangeException">Thrown when there are not enough bits in the stream to align the position and read a byte.</exception>
-        public byte NextAlignedByte(){
+        public virtual byte NextAlignedByte(){
             AlignToByteBoundary();
             return (byte)NextChunk(ByteSize);
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Allows decorating another <see cref="BitReader" /> with new functionality.
+        /// </summary>
+        internal abstract class Wrapped : BitReader{
+            protected override long Index => wrapped.Index;
+
+            private readonly BitReader wrapped;
+
+            protected Wrapped(BitReader wrapped){
+                this.wrapped = wrapped;
+            }
+
+            public override bool NextBit(){
+                return wrapped.NextBit();
+            }
+
+            public override int NextChunk(int bits){
+                return wrapped.NextChunk(bits);
+            }
+
+            public override void AlignToByteBoundary(){
+                wrapped.AlignToByteBoundary();
+            }
+
+            public override byte NextAlignedByte(){
+                return wrapped.NextAlignedByte();
+            }
         }
     }
 }
