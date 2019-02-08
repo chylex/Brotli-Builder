86e25692b8e8a9d6314b6382b7bea1216c12d4c6
chylex <contact@chylex.com>
2019-02-08 10:29:03 +0100

    Implement Brotli compressed meta-block contents and insert&copy command serialization

---
+++ BrotliLib/Brotli/Components/Contents/Compressed/InsertCopyCommand.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using BrotliLib.Brotli.Components.Data;
+using BrotliLib.Brotli.Components.Utils;
+using BrotliLib.IO;
+
+namespace BrotliLib.Brotli.Components.Contents.Compressed{
+    public sealed class InsertCopyCommand{
+        public const int ImpliedDistanceCodeZero = int.MaxValue;
+
+        public InsertCopyLengths Lengths { get; }
+        public IReadOnlyList<byte> Literals { get; }
+        public int? CopyDistance { get; }
+
+        public InsertCopyCommand(IReadOnlyList<byte> literals, int copyLength){
+            this.Lengths = new InsertCopyLengths(literals.Count, copyLength);
+            this.Literals = literals;
+            this.CopyDistance = null;
+        }
+
+        public InsertCopyCommand(IReadOnlyList<byte> literals, int copyLength, int copyDistance){
+            this.Lengths = new InsertCopyLengths(literals.Count, copyLength);
+            this.Literals = literals;
+            this.CopyDistance = copyDistance;
+        }
+
+        // Serialization
+
+        internal static readonly IBitSerializer<InsertCopyCommand, CompressedMetaBlockContents.DataContext> Serializer = new BitSerializer<InsertCopyCommand, CompressedMetaBlockContents.DataContext>(
+            fromBits: (reader, context) => {
+                MetaBlockCompressionHeader header = context.Header;
+                BrotliGlobalState state = context.State;
+                
+                // Insert&copy lengths
+                
+                int icBlockID = context.NextBlockID(Category.InsertCopy);
+                InsertCopyLengthCode icCode = header.InsertCopyTrees[icBlockID].Root.LookupValue(reader);
+                InsertCopyLengths icValues = InsertCopyLengths.Serializer.FromBits(reader, icCode);
+
+                int insertLength = icValues.InsertLength;
+                int copyLength = icValues.CopyLength;
+                
+                // Literals
+
+                byte[] literals = new byte[insertLength];
+                
+                for(int insertIndex = 0; insertIndex < insertLength; insertIndex++){
+                    int blockID = context.NextBlockID(Category.Literal);
+                    int contextID = context.NextLiteralContextID(header.LiteralCtxModes[blockID]);
+                    int treeID = header.LiteralCtxMap.DetermineTreeID(blockID, contextID);
+
+                    byte literal = header.LiteralTrees[treeID].Root.LookupValue(reader);
+
+                    literals[insertIndex] = literal;
+                    context.WriteLiteral(literal);
+                }
+
+                if (!context.NeedsMoreData){
+                    return new InsertCopyCommand(literals, copyLength);
+                }
+
+                // Distance
+                
+                int distanceValue;
+                bool useDistanceCodeZero = icCode.UseDistanceCodeZero;
+
+                if (useDistanceCodeZero){
+                    distanceValue = state.DistanceBuffer.Front;
+                }
+                else{
+                    int blockID = context.NextBlockID(Category.Distance);
+                    int contextID = Math.Min(3, copyLength - 2);
+                    int treeID = header.DistanceCtxMap.DetermineTreeID(blockID, contextID);
+
+                    DistanceCode distanceCode = header.DistanceTrees[treeID].Root.LookupValue(reader);
+                    distanceValue = DistanceCode.Serializer.FromBits(reader, distanceCode.MakeContext(state));
+                }
+
+                context.WriteCopy(copyLength, distanceValue, useDistanceCodeZero);
+
+                return new InsertCopyCommand(literals, copyLength, useDistanceCodeZero ? ImpliedDistanceCodeZero : distanceValue);
+            },
+
+            toBits: (writer, obj, context) => {
+                MetaBlockCompressionHeader header = context.Header;
+                BrotliGlobalState state = context.State;
+                
+                bool endsAfterLiterals = obj.CopyDistance == null;
+                bool useDistanceCodeZero = obj.CopyDistance == ImpliedDistanceCodeZero;
+
+                // Insert&copy lengths
+
+                InsertCopyLengths icLengths = obj.Lengths;
+                int icBlockID = context.NextBlockID(Category.InsertCopy);
+                var icEntry = header.InsertCopyTrees[icBlockID].FindEntry(code => icLengths.CanEncodeUsing(code) && (useDistanceCodeZero == code.UseDistanceCodeZero || endsAfterLiterals));
+                var icCode = icEntry.Key;
+
+                writer.WriteBits(icEntry.Value);
+                InsertCopyLengths.Serializer.ToBits(writer, icLengths, icCode);
+                
+                // Literals
+                
+                foreach(byte literal in obj.Literals){
+                    int blockID = context.NextBlockID(Category.Literal);
+                    int contextID = context.NextLiteralContextID(header.LiteralCtxModes[blockID]);
+                    int treeID = header.LiteralCtxMap.DetermineTreeID(blockID, contextID);
+
+                    writer.WriteBits(header.LiteralTrees[treeID].FindPath(literal));
+                    context.WriteLiteral(literal);
+                }
+
+                // Distance
+
+                if (endsAfterLiterals){
+                    return;
+                }
+
+                int copyDistance;
+
+                if (useDistanceCodeZero){
+                    copyDistance = state.DistanceBuffer.Front;
+                }
+                else{
+                    copyDistance = obj.CopyDistance.Value;
+
+                    int blockID = context.NextBlockID(Category.Distance);
+                    int contextID = Math.Min(3, icLengths.CopyLength - 2);
+                    int treeID = header.DistanceCtxMap.DetermineTreeID(blockID, contextID);
+
+                    var distanceEntry = header.DistanceTrees[treeID].FindEntry(code => code.CanEncodeValue(state, copyDistance));
+                    var distanceCode = distanceEntry.Key;
+
+                    writer.WriteBits(distanceEntry.Value);
+                    DistanceCode.Serializer.ToBits(writer, copyDistance, distanceCode.MakeContext(state));
+                }
+                
+                context.WriteCopy(icLengths.CopyLength, copyDistance, useDistanceCodeZero);
+            }
+        );
+    }
+}
--- BrotliLib/Brotli/Components/Contents/CompressedMetaBlockContents.cs
+++ BrotliLib/Brotli/Components/Contents/CompressedMetaBlockContents.cs
@@ -1,18 +1,146 @@
-﻿using BrotliLib.IO;
+﻿using System;
+using System.Collections.Generic;
+using BrotliLib.Brotli.Components.Contents.Compressed;
+using BrotliLib.Brotli.Components.Data;
+using BrotliLib.Brotli.Components.Utils;
+using BrotliLib.Collections;
+using BrotliLib.IO;
+using BlockSwitchCommandMap = BrotliLib.Brotli.Components.Utils.CategoryMap<System.Collections.Generic.IReadOnlyList<BrotliLib.Brotli.Components.Contents.Compressed.BlockSwitchCommand>>;
 
 namespace BrotliLib.Brotli.Components.Contents{
     public sealed class CompressedMetaBlockContents{
+        public MetaBlockCompressionHeader Header { get; }
+        public IReadOnlyList<InsertCopyCommand> InsertCopyCommands { get; }
+        public BlockSwitchCommandMap BlockSwitchCommands { get; }
+
+        public CompressedMetaBlockContents(MetaBlockCompressionHeader header, IReadOnlyList<InsertCopyCommand> insertCopyCommands, BlockSwitchCommandMap blockSwitchCommands){
+            this.Header = header;
+            this.InsertCopyCommands = insertCopyCommands;
+            this.BlockSwitchCommands = blockSwitchCommands;
+        }
+
+        // Context
+
+        internal abstract class DataContext : MetaBlock.Context{
+            public MetaBlockCompressionHeader Header { get; }
+
+            public bool NeedsMoreData => bytesWritten < MetaBlock.DataLength.UncompressedBytes;
+
+            protected readonly CategoryMap<BlockTypeTracker> blockTrackers;
+            private readonly RingBuffer<byte> recentOutput;
+            private int bytesWritten;
+
+            protected DataContext(MetaBlock.Context wrapped, MetaBlockCompressionHeader header) : base(wrapped.MetaBlock, wrapped.State){
+                this.Header = header;
+                this.blockTrackers = Header.BlockTypes.Select((_, info) => new BlockTypeTracker(info));
+                this.recentOutput = new RingBuffer<byte>(0, 0);
+            }
+
+            public abstract int NextBlockID(Category category);
+
+            public int NextLiteralContextID(LiteralContextMode mode){
+                return mode.DetermineContextID(recentOutput.Front, recentOutput.Back);
+            }
+
+            public void WriteLiteral(byte literal){
+                State.Output(literal);
+                recentOutput.Push(literal);
+                ++bytesWritten;
+            }
+
+            public void WriteCopy(int copyLength, int copyDistance, bool usedDistanceCodeZero){
+                if (copyDistance < 0){
+                    throw new InvalidOperationException("Distance cannot be negative.");
+                }
+
+                int maxDistance = State.MaxDistance;
+
+                if (copyDistance <= maxDistance){
+                    if (copyDistance != 0 && !usedDistanceCodeZero){
+                        State.DistanceBuffer.Push(copyDistance);
+                    }
+
+                    for(int index = 0; index < copyLength; index++){
+                        State.Output(State.GetByteAt(State.OutputSize - copyDistance));
+                    }
+
+                    bytesWritten += copyLength;
+                }
+                else{
+                    byte[] word = State.Dictionary.ReadTransformed(copyLength, copyDistance - maxDistance - 1);
+
+                    State.Output(word);
+                    bytesWritten += word.Length;
+                }
+
+                recentOutput.Push(State.GetByteAt(State.OutputSize - 2));
+                recentOutput.Push(State.GetByteAt(State.OutputSize - 1));
+            }
+        }
+
+        private class ReaderDataContext : DataContext{
+            public BlockSwitchCommandMap BlockSwitchCommands => blockSwitchCommands.Select<IReadOnlyList<BlockSwitchCommand>>((_, list) => list.AsReadOnly());
+            
+            private readonly BitReader reader;
+            private readonly CategoryMap<List<BlockSwitchCommand>> blockSwitchCommands;
+
+            public ReaderDataContext(MetaBlock.Context wrapped, MetaBlockCompressionHeader header, BitReader reader) : base(wrapped, header){
+                this.reader = reader;
+                this.blockSwitchCommands = new CategoryMap<List<BlockSwitchCommand>>(_ => new List<BlockSwitchCommand>());
+            }
+
+            public override int NextBlockID(Category category){
+                BlockTypeTracker tracker = blockTrackers[category];
+                BlockSwitchCommand nextCommand = tracker.ReadCommand(reader);
+
+                if (nextCommand != null){
+                    blockSwitchCommands[category].Add(nextCommand);
+                }
+
+                return tracker.CurrentID;
+            }
+        }
+
+        private class WriterDataContext : DataContext{
+            private readonly BitWriter writer;
+            private readonly CategoryMap<Queue<BlockSwitchCommand>> blockSwitchQueues;
+
+            public WriterDataContext(MetaBlock.Context wrapped, MetaBlockCompressionHeader header, BlockSwitchCommandMap blockSwitchCommands, BitWriter writer) : base(wrapped, header){
+                this.writer = writer;
+                this.blockSwitchQueues = blockSwitchCommands.Select((_, list) => new Queue<BlockSwitchCommand>(list));
+            }
+
+            public override int NextBlockID(Category category){
+                BlockTypeTracker tracker = blockTrackers[category];
+                tracker.WriteCommand(writer, blockSwitchQueues[category]);
+                return tracker.CurrentID;
+            }
+        }
 
         // Serialization
 
         internal static readonly IBitSerializer<CompressedMetaBlockContents, MetaBlock.Context> Serializer = new BitSerializer<CompressedMetaBlockContents, MetaBlock.Context>(
             fromBits: (reader, context) => {
-                // TODO
-                return new CompressedMetaBlockContents();
+                MetaBlockCompressionHeader header = MetaBlockCompressionHeader.Serializer.FromBits(reader, context);
+
+                ReaderDataContext dataContext = new ReaderDataContext(context, header, reader);
+                List<InsertCopyCommand> icCommands = new List<InsertCopyCommand>();
+                
+                do{
+                    icCommands.Add(InsertCopyCommand.Serializer.FromBits(reader, dataContext));
+                }while(dataContext.NeedsMoreData);
+                
+                return new CompressedMetaBlockContents(header, icCommands, dataContext.BlockSwitchCommands);
             },
 
             toBits: (writer, obj, context) => {
-                // TODO
+                MetaBlockCompressionHeader.Serializer.ToBits(writer, obj.Header, context);
+                
+                DataContext dataContext = new WriterDataContext(context, obj.Header, obj.BlockSwitchCommands, writer);
+
+                foreach(InsertCopyCommand icCommand in obj.InsertCopyCommands){
+                    InsertCopyCommand.Serializer.ToBits(writer, icCommand, dataContext);
+                }
             }
         );
     }
