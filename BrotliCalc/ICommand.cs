335e52b10c3dee68b2bcf367db75bef93f94ca85
chylex <contact@chylex.com>
2019-03-10 15:05:16 +0100

    Add BrotliCalc project w/ simple console command interface

---
+++ BrotliCalc/ICommand.cs
@@ -0,0 +1,9 @@
+﻿namespace BrotliCalc{
+    interface ICommand{
+        string FullName { get; }
+        string ShortName { get; }
+        string Arguments { get; }
+
+        string Process(string[] args);
+    }
+}
---
+++ BrotliCalc/Program.cs
@@ -0,0 +1,109 @@
+﻿using BrotliCalc.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace BrotliCalc{
+    public static class Program{
+        private static readonly List<ICommand> Commands = new List<ICommand>{
+        };
+
+        private static void Main(string[] args){
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Available commands:");
+            Console.WriteLine();
+            
+            foreach(ICommand command in Commands){
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write($" {command.ShortName} / {command.FullName} ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(command.Arguments);
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine();
+            Console.WriteLine("Type r/repeat to repeat the last command.");
+            Console.WriteLine("Type q/quit to close the program.");
+            Console.WriteLine();
+
+            string[] lastInput = null;
+
+            while(true){
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("> ");
+
+                string[] input = Console.ReadLine().Split(new []{ ' ' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                
+                if (input.Length == 0){
+                    continue;
+                }
+
+                Console.ResetColor();
+                Console.WriteLine();
+
+                string trigger = input[0];
+
+                if ((trigger == "r" || trigger == "repeat") && lastInput != null){
+                    input = lastInput;
+                    trigger = input[0];
+                }
+                else if (trigger == "q" || trigger == "quit"){
+                    break;
+                }
+                
+                var command = Commands.FirstOrDefault(cmd1 => trigger == cmd1.FullName) ?? Commands.FirstOrDefault(cmd2 => trigger == cmd2.ShortName);
+
+                if (command == null){
+                    Console.WriteLine("Command not found.");
+                }
+                else{
+                    lastInput = input;
+
+                    try{
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.WriteLine(command.Process(ParseCommandArguments(input.ElementAtOrDefault(1) ?? string.Empty)));
+                    }catch(Exception e){
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Error processing the command:");
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine(e.Message);
+                        Debug.Print(e.ToString());
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static string[] ParseCommandArguments(string input){
+            var chars = input.ToCharArray();
+
+            bool inSingleQuote = false;
+            bool inDoubleQuote = false;
+
+            for(int index = 0; index < chars.Length; index++){
+                var ch = chars[index];
+
+                if (ch == '"' && !inSingleQuote){
+                    inDoubleQuote = !inDoubleQuote;
+                    chars[index] = '\n';
+                }
+                else if (ch == '\'' && !inDoubleQuote){
+                    inSingleQuote = !inSingleQuote;
+                    chars[index] = '\n';
+                }
+                else if (ch == ' ' && !inSingleQuote && !inDoubleQuote){
+                    chars[index] = '\n';
+                }
+            }
+
+            return new string(chars).Split(new []{ '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
