d21af052804e044b7881a1d1858edfb38479b59b
chylex <contact@chylex.com>
2019-03-11 10:18:08 +0100

    Remove references to siblings in MarkerNode

--- BrotliLib/Markers/MarkerNode.cs
+++ BrotliLib/Markers/MarkerNode.cs
@@ -1,47 +1,28 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace BrotliLib.Markers{
     public sealed class MarkerNode : IEnumerable<MarkerNode>{
         public MarkerNode Parent { get; private set; }
-        public MarkerNode PrevSibling { get; private set; }
-        public MarkerNode NextSibling { get; private set; }
 
         public int Depth { get; set; }
         public Marker Marker { get; set; }
 
-        private readonly List<MarkerNode> children = new List<MarkerNode>();
+        private readonly List<MarkerNode> children = new List<MarkerNode>(2);
 
         internal void AddChildOrSibling(MarkerNode added){
             if (Depth == added.Depth){
-                MarkerNode myLastSibling = this;
-                MarkerNode testSibling;
-
-                while((testSibling = myLastSibling.NextSibling) != null){
-                    myLastSibling = testSibling;
-                }
-
                 added.Parent = Parent;
-                added.PrevSibling = myLastSibling;
-                myLastSibling.NextSibling = added;
+                Parent.children.Add(added);
             }
             else if (Depth < added.Depth){
-                MarkerNode myLastChild = children.LastOrDefault();
-
                 added.Parent = this;
-
-                if (myLastChild != null){
-                    added.PrevSibling = myLastChild;
-                    myLastChild.NextSibling = added;
-                }
+                children.Add(added);
             }
             else{
                 throw new InvalidOperationException();
             }
-
-            added.Parent?.children?.Add(added);
         }
 
         public IEnumerator<MarkerNode> GetEnumerator(){
