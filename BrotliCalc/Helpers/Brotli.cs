a33f7c11812ba411cd681e70bf528a38ffec3ba3
chylex <contact@chylex.com>
2019-03-26 02:57:43 +0100

    Add utilities for mass (de)compression and CSVs to BrotliCalc

---
+++ BrotliCalc/Helpers/Brotli.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using BrotliLib.Brotli;
+using BrotliLib.Brotli.Components;
+
+namespace BrotliCalc.Helpers{
+    static class Brotli{
+        private const int CompressionFileLimit = 2000;
+        private const string CompressedFileExtension = ".br";
+
+        public static IEnumerable<(string name, BrotliFileStructure bfs)> DecompressPath(string path, string pattern = "*" + CompressedFileExtension){
+            int fullPathLength = Path.GetFullPath(path).Length;
+
+            (string, BrotliFileStructure) ReadFile(string file){
+                Debug.WriteLine($"Decompressing file {file}...");
+                return (file.Substring(fullPathLength), BrotliFileStructure.FromBytes(File.ReadAllBytes(file)));
+            }
+
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory)){
+                return Directory.EnumerateFiles(path, pattern, SearchOption.AllDirectories).Select(ReadFile);
+            }
+            else{
+                return new []{ ReadFile(path) };
+            }
+        }
+
+        public static int CompressPath(string path, int quality, WindowSize windowSize){
+            if (!File.Exists("dict")){
+                throw new FileNotFoundException("Dictionary file must be named 'dict' and placed into the working directory.", "dict");
+            }
+
+            if (quality < 0 || quality > 11){
+                throw new ArgumentOutOfRangeException(nameof(quality), "Compression quality must be in range [0; 11].");
+            }
+
+            IList<string> filePaths;
+            
+            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory)){
+                filePaths = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories).Where(file => Path.GetExtension(file) != CompressedFileExtension).ToList();
+            }
+            else{
+                filePaths = new []{ path };
+            }
+
+            if (filePaths.Count > CompressionFileLimit){
+                throw new InvalidOperationException($"Too many files to process ({filePaths.Count} > {CompressionFileLimit}), cancelling command for safety.");
+            }
+
+            string argPrefix = $"-w {windowSize.Bits} -q {quality} -S .{quality}{CompressedFileExtension} -f ";
+
+            try{
+                foreach(string file in filePaths){
+                    Debug.WriteLine($"Compressing file {file}...");
+
+                    using(Process process = Process.Start("brotli", argPrefix + '"' + file + '"')){
+                        process.Start();
+                        process.WaitForExit();
+                    }
+
+                    // TODO add multiprocess support
+                }
+            }catch(Win32Exception e){
+                throw new InvalidOperationException("Brotli executable must be named 'brotli' and placed into the working directory or environment path.", e);
+            }
+
+            return filePaths.Count;
+        }
+    }
+}
---
+++ BrotliCalc/Helpers/Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BrotliCalc.Helpers{
+    sealed class Table{
+        private readonly string[] columns;
+        private readonly List<string[]> rows = new List<string[]>();
+
+        public Table(string[] columns){
+            this.columns = columns;
+        }
+
+        public void AddRow(params object[] values){
+            if (values.Length != columns.Length){
+                throw new ArgumentException("Amount of entries must match the amount of columns (" + values.Length + " != " + columns.Length + ").", nameof(values));
+            }
+
+            rows.Add(values.Select(value => value.ToString()).ToArray());
+        }
+
+        public void WriteCSV(string path){
+            using(StreamWriter writer = new StreamWriter(path)){
+                writer.WriteLine(string.Join(',', columns));
+
+                foreach(string[] row in rows){
+                    writer.Write('"');
+                    writer.WriteLine(string.Join("\",\"", row));
+                    writer.Write('"');
+                }
+            }
+        }
+    }
+}
