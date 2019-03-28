caf2ca9c1909f303fee3aca9c7621b639ba68db0
chylex <contact@chylex.com>
2019-03-28 04:06:11 +0100

    Implement window output strategy for when full output isn't necessary

---
+++ BrotliLib/Brotli/State/Output/BrotliOutputWindowed.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using BrotliLib.Brotli.Components;
+using BrotliLib.Collections;
+
+namespace BrotliLib.Brotli.State.Output{
+    /// <summary>
+    /// Stores only enough output to reference bytes inside the sliding window.
+    /// </summary>
+    public class BrotliOutputWindowed : IBrotliOutputState{
+        public int OutputSize { get; private set; }
+
+        private readonly RingBuffer<byte> windowBuffer;
+
+        public BrotliOutputWindowed(WindowSize windowSize){
+            this.windowBuffer = new RingBuffer<byte>(new byte[windowSize.Bytes]);
+        }
+
+        private BrotliOutputWindowed(BrotliOutputWindowed original){
+            this.windowBuffer = new RingBuffer<byte>(original.windowBuffer);
+            this.OutputSize = original.OutputSize;
+        }
+
+        public void Write(byte value){
+            windowBuffer.Push(value);
+            ++OutputSize;
+        }
+
+        public void Write(byte[] bytes){
+            foreach(byte value in bytes){
+                windowBuffer.Push(value);
+            }
+
+            OutputSize += bytes.Length;
+        }
+
+        public byte GetByte(int distance){
+            int bufferLength = windowBuffer.Length;
+            int maxDistance = Math.Min(bufferLength, OutputSize);
+
+            if (distance > maxDistance){
+                throw new ArgumentOutOfRangeException(nameof(distance), "Distance is out of range: " + distance + " > " + maxDistance);
+            }
+
+            return windowBuffer[bufferLength - distance];
+        }
+
+        public IBrotliOutputState Clone(){
+            return new BrotliOutputWindowed(this);
+        }
+    }
+}
