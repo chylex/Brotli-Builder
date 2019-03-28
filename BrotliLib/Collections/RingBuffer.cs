4ec79e2d5fa394ab4846c81b1fda087b7d22753c
chylex <contact@chylex.com>
2019-03-28 03:31:28 +0100

    Add cloning constructor to RingBuffer

--- BrotliLib/Collections/RingBuffer.cs
+++ BrotliLib/Collections/RingBuffer.cs
@@ -50,6 +50,13 @@ namespace BrotliLib.Collections{
         }
 
         /// <summary>
+        /// Initializes a new ring buffer, with a shallow copy of the contents of the provided <paramref name="original"/>.
+        /// </summary>
+        public RingBuffer(RingBuffer<T> original){
+            this.values = (T[])original.values.Clone();
+        }
+
+        /// <summary>
         /// Pushes a new value to the front of the queue, removing the value at the back.
         /// </summary>
         public void Push(T value){
