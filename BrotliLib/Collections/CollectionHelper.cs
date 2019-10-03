65dbe29dc6d2e1e0d139d5e7a83b42a45ab08b7b
chylex <contact@chylex.com>
2019-10-03 21:23:02 +0200

    Add a few utilities to CollectionHelper (ContainsAt, Skip)

--- BrotliLib/Collections/CollectionHelper.cs
+++ BrotliLib/Collections/CollectionHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BrotliLib.Collections{
     public static class CollectionHelper{
@@ -10,6 +11,30 @@ namespace BrotliLib.Collections{
 
         public static byte[] Clone(byte[] input){
             return Slice(input, 0, input.Length);
+        }
+
+        public static bool ContainsAt(byte[] input, int start, byte[] contents){
+            if (contents.Length == 0){
+                return true;
+            }
+
+            if (input.Length - start < contents.Length){
+                return false;
+            }
+
+            for(int offset = 0; offset < contents.Length; offset++){
+                if (input[start + offset] != contents[offset]){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static IEnumerable<T> Skip<T>(T[] array, int start){
+            for(int index = start; index < array.Length; index++){
+                yield return array[index];
+            }
         }
     }
 }
