chylex/Brotli-Builder

commit d8a67133921d138d9d69823c73d83f928947144d

Fix broken generation of explicit distance codes 0

BrotliLib/Brotli/Encode/CompressedMetaBlockBuilder.cs
@@ -129,7 +129,7 @@ namespace BrotliLib.Brotli.Encode{
                 }
                 else{
                     var distanceCodes = icCommand.CopyDistance.MakeCode(DistanceParameters, state);
-                    DistanceCode distanceCode = null;
+                    DistanceCode distanceCode;
                     
                     if (distanceCodes != null){
                         int blockID = NextBlockID(Category.Distance);
@@ -139,11 +139,17 @@ namespace BrotliLib.Brotli.Encode{
                         var codeList = distanceCodeFreq[treeID];
                         codeList.Add(distanceCode = distanceCodes.FirstOrDefault(codeList.Contains) ?? distanceCodes[0]); // TODO figure out a better strategy for picking the code
                     }
+                    else{
+                        throw new InvalidOperationException("Could not generate distance codes for command " + icCommand + ".");
+                    }
                     
-                    icLengthCode = icLengthValues.MakeCode(distanceCode == null || distanceCode.Equals(DistanceCode.Zero) ? DistanceCodeZeroStrategy.PreferEnabled : DistanceCodeZeroStrategy.Disable);
+                    icLengthCode = icLengthValues.MakeCode(distanceCode.Equals(DistanceCode.Zero) ? DistanceCodeZeroStrategy.PreferEnabled : DistanceCodeZeroStrategy.Disable);
 
                     if (icLengthCode.UseDistanceCodeZero){
-                        icCommand = icCommand.WithImplicitDistanceCodeZero();
+                        icCommand = icCommand.WithDistance(DistanceInfo.ImplicitCodeZero);
+                    }
+                    else if (distanceCode.Equals(DistanceCode.Zero)){
+                        icCommand = icCommand.WithDistance(DistanceInfo.ExplicitCodeZero);
                     }
 
                     state.OutputCopy(icCommand.CopyLength, icCommand.CopyDistance);

BrotliLib/Brotli/Components/Contents/Compressed/InsertCopyCommand.cs
@@ -33,8 +33,8 @@ namespace BrotliLib.Brotli.Components.Contents.Compressed{
             this.CopyDistance = newDistanceInfo;
         }
 
-        public InsertCopyCommand WithImplicitDistanceCodeZero(){
-            return CopyDistance == DistanceInfo.ImplicitCodeZero ? this : new InsertCopyCommand(this, DistanceInfo.ImplicitCodeZero);
+        public InsertCopyCommand WithDistance(DistanceInfo newDistanceInfo){
+            return CopyDistance == newDistanceInfo ? this : new InsertCopyCommand(this, newDistanceInfo);
         }
 
         // Object
@@ -46,7 +46,7 @@ namespace BrotliLib.Brotli.Components.Contents.Compressed{
                    CopyDistance == command.CopyDistance;
         }
 
-        public override int GetHashCode() {
+        public override int GetHashCode(){
             unchecked{
                 var hashCode = -1468049732;
                 hashCode = hashCode * -1521134295 + EqualityComparer<IReadOnlyList<Literal>>.Default.GetHashCode(Literals);

commit 8ecdd90caab8487bc23a61122437e063673960fc

Fix recently introduced crash w/ implicit distance code zero
Caused by d8a67133921d138d9d69823c73d83f928947144d

BrotliLib/Brotli/Encode/CompressedMetaBlockBuilder.cs
@@ -128,8 +128,8 @@ namespace BrotliLib.Brotli.Encode{
                     icLengthCode = icLengthValues.MakeCode(DistanceCodeZeroStrategy.PreferEnabled); // TODO good strategy?
                 }
                 else{
+                    DistanceCode distanceCode = null;
                     var distanceCodes = icCommand.CopyDistance.MakeCode(DistanceParameters, state);
-                    DistanceCode distanceCode;
                     
                     if (distanceCodes != null){
                         int blockID = NextBlockID(Category.Distance);
@@ -139,16 +139,20 @@ namespace BrotliLib.Brotli.Encode{
                         var codeList = distanceCodeFreq[treeID];
                         codeList.Add(distanceCode = distanceCodes.FirstOrDefault(codeList.Contains) ?? distanceCodes[0]); // TODO figure out a better strategy for picking the code
                     }
-                    else{
-                        throw new InvalidOperationException("Could not generate distance codes for command " + icCommand + ".");
-                    }
-                    
-                    icLengthCode = icLengthValues.MakeCode(distanceCode.Equals(DistanceCode.Zero) ? DistanceCodeZeroStrategy.PreferEnabled : DistanceCodeZeroStrategy.Disable);
+
+                    bool isImplicitCodeZero = distanceCode == null;
+                    bool isDistanceCodeZero = isImplicitCodeZero || distanceCode.Equals(DistanceCode.Zero);
+
+                    icLengthCode = icLengthValues.MakeCode(
+                        isImplicitCodeZero ? DistanceCodeZeroStrategy.ForceEnabled :
+                        isDistanceCodeZero ? DistanceCodeZeroStrategy.PreferEnabled :
+                                             DistanceCodeZeroStrategy.Disable
+                    );
 
                     if (icLengthCode.UseDistanceCodeZero){
                         icCommand = icCommand.WithDistance(DistanceInfo.ImplicitCodeZero);
                     }
-                    else if (distanceCode.Equals(DistanceCode.Zero)){
+                    else if (isDistanceCodeZero){
                         icCommand = icCommand.WithDistance(DistanceInfo.ExplicitCodeZero);
                     }
 