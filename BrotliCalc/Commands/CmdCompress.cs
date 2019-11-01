0e22792868c9dd57eece58cba662340ddebf9edf
chylex <contact@chylex.com>
2019-11-01 19:10:48 +0100

    Move BrotliCalc compression code to its command & parallelize it

--- BrotliCalc/Commands/CmdCompress.cs
+++ BrotliCalc/Commands/CmdCompress.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using BrotliCalc.Helpers;
 using BrotliLib.Brotli.Components;
 using BrotliLib.Numbers;
+using DiagProcess = System.Diagnostics.Process;
 
 namespace BrotliCalc.Commands{
     class CmdCompress : ICommand{
@@ -12,17 +15,51 @@ namespace BrotliCalc.Commands{
         public string ArgumentDesc => "<source-path> <quality|all> [window-size]";
         public IntRange ArgumentCount => new IntRange(2, 3);
 
-        public string Process(string[] args){
-            var qualities = args[1] == "all" ? Enumerable.Range(0, 12) : Enumerable.Range(int.Parse(args[1]), 1);
-            var windowSize = args.Length >= 3 ? new WindowSize(int.Parse(args[2])) : WindowSize.Default;
+        private static readonly IntRange QualityRange = new IntRange(0, 11);
+        private const int FileLimit = 2000;
 
-            int totalFiles = qualities.Sum(quality => Brotli.CompressPath(args[0], quality, windowSize));
-            
-            if (totalFiles > 0){
-                Console.WriteLine();
+        public string Process(string[] args){
+            var path = args[0];
+            var qualities = args[1] == "all" ? QualityRange : IntRange.Only(int.Parse(args[1]));
+            var wbits = (args.Length >= 3 ? new WindowSize(int.Parse(args[2])) : WindowSize.Default).Bits;
+
+            if (!qualities.Values.All(QualityRange.Contains)){
+                throw new ArgumentException($"Compression quality must be in range {QualityRange}.");
             }
-            
+
+            if (!File.Exists("dict")){
+                throw new FileNotFoundException("Dictionary file must be named 'dict' and placed into the working directory.", "dict");
+            }
+
+            var groups = Brotli.ListPath(path).ToArray();
+            int totalFiles = groups.Length;
+
+            if (totalFiles > FileLimit){
+                throw new InvalidOperationException($"Too many files to process ({totalFiles} > {FileLimit}), cancelling command for safety.");
+            }
+
+            var items = qualities.Values.Cartesian(groups).ToArray();
+
+            using(var progress = new Progress(items.Length)){
+                try{
+                    items.Parallelize().ForAll(item => {
+                        var (quality, group) = item;
+                        var file = group.Uncompressed;
+
+                        Compress(wbits, quality, file.Path);
+                        progress.Post($"Finished {file} (quality {quality})");
+                    });
+                }catch(Win32Exception e){
+                    throw new InvalidOperationException("Brotli executable must be named 'brotli' and placed into the working directory or environment path.", e);
+                }
+            }
+
             return $"Compressed {totalFiles} file(s).";
+        }
+
+        private static void Compress(int wbits, int quality, string path){
+            using DiagProcess process = DiagProcess.Start("brotli", $"-w {wbits} -q {quality} -S .{quality}{Brotli.CompressedFileExtension} -f \"{path}\"");
+            process.WaitForExit();
         }
     }
 }
--- BrotliCalc/Helpers/Brotli.cs
+++ BrotliCalc/Helpers/Brotli.cs
@@ -1,23 +1,15 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using BrotliLib.Brotli.Components;
-using BrotliLib.Numbers;
 
 namespace BrotliCalc.Helpers{
     static class Brotli{
-        private const int CompressionFileLimit = 2000;
-        private const string CompressedFileExtension = ".br";
+        public const string CompressedFileExtension = ".br";
         public const char DirectorySeparator = '/';
         
         private static readonly Regex RegexCompressionIdentifier = new Regex(@"\.([^.]+)\.br$");
-        private static readonly IntRange QualityRange = new IntRange(0, 11);
 
         private static string GetUncompressedName(string path){
             return Path.GetExtension(path) == CompressedFileExtension ? RegexCompressionIdentifier.Replace(path, "") : path;
@@ -63,53 +55,6 @@ namespace BrotliCalc.Helpers{
             }
 
             return Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories).GroupBy(GetUncompressedName).Select(ProcessGroup);
-        }
-
-        public static int CompressPath(string path, int quality, WindowSize windowSize){
-            if (!File.Exists("dict")){
-                throw new FileNotFoundException("Dictionary file must be named 'dict' and placed into the working directory.", "dict");
-            }
-
-            if (!QualityRange.Contains(quality)){
-                throw new ArgumentOutOfRangeException(nameof(quality), $"Compression quality must be in range {QualityRange}.");
-            }
-
-            BrotliFileGroup[] groups;
-            
-            if (File.GetAttributes(path).HasFlag(FileAttributes.Directory)){
-                groups = ListPath(path).ToArray();
-            }
-            else{
-                groups = new BrotliFileGroup[]{
-                    new BrotliFileGroup(new BrotliFile.Uncompressed(path, Path.GetFileName(path)), new BrotliFile.Compressed[0])
-                };
-            }
-
-            if (groups.Length > CompressionFileLimit){
-                throw new InvalidOperationException($"Too many files to process ({groups.Length} > {CompressionFileLimit}), cancelling command for safety.");
-            }
-
-            string argPrefix = $"-w {windowSize.Bits} -q {quality} -S .{quality}{CompressedFileExtension} -f ";
-
-            try{
-                Parallel.ForEach(Partitioner.Create(groups, EnumerablePartitionerOptions.NoBuffering), (group, state) => {
-                    var file = group.Uncompressed;
-
-                    using Process process = Process.Start("brotli", argPrefix + '"' + file.Path + '"');
-                    process.WaitForExit();
-
-                    Console.WriteLine($"Finished {file.Name} (quality {quality}).");
-                });
-            }catch(AggregateException e){
-                if (e.InnerException is Win32Exception we){
-                    throw new InvalidOperationException("Brotli executable must be named 'brotli' and placed into the working directory or environment path.", we);
-                }
-                else{
-                    throw;
-                }
-            }
-
-            return groups.Length;
         }
 
         public static IEnumerable<(BrotliFileGroup, BrotliFile.Uncompressed)> SelectUncompressedFiles(this IEnumerable<BrotliFileGroup> me){
