70cf0d6b14035a4d97136ab87a239ca2292b2cb8
chylex <contact@chylex.com>
2020-01-29 23:12:36 +0100

    Tweak BrotliCalc CSV generation to omit quotes for numeric values

--- BrotliCalc/Helpers/Table.cs
+++ BrotliCalc/Helpers/Table.cs
@@ -11,17 +11,32 @@ namespace BrotliCalc.Helpers{
             this.columns = columns;
         }
 
-        protected IEnumerable<string> ReadRowAsString(object?[] values){
+        protected IEnumerable<string> ReadRowAsStrings(object?[] values){
             if (values.Length != columns.Count){
-                throw new ArgumentException("Amount of entries must match the amount of columns (" + values.Length + " != " + columns.Count + ").", nameof(values));
+                throw new ArgumentException($"Amount of entries must match the amount of columns ({values.Length} != {columns.Count}).", nameof(values));
             }
 
-            return values.Select(value => value?.ToString() ?? "?");
+            return values.Select(RowValueToString);
         }
 
         public abstract void AddRow(params object?[] values);
+        protected abstract string RowValueToString(object? value);
 
         internal class CSV : Table, IDisposable{
+            private static readonly HashSet<TypeCode> OmitQuotes = new HashSet<TypeCode>{
+                TypeCode.SByte, TypeCode.Byte,
+                TypeCode.Int16, TypeCode.UInt16,
+                TypeCode.Int32, TypeCode.UInt32,
+                TypeCode.Int64, TypeCode.UInt64,
+                TypeCode.Single,
+                TypeCode.Double,
+                TypeCode.Decimal
+            };
+
+            private static bool CanOmitQuotes(object value){
+                return OmitQuotes.Contains(Type.GetTypeCode(value.GetType()));
+            }
+
             private readonly StreamWriter writer;
 
             public CSV(string path, string[] columns) : base(columns){
@@ -30,10 +45,12 @@ namespace BrotliCalc.Helpers{
                 this.writer.Flush();
             }
 
+            protected override string RowValueToString(object? value){
+                return value == null ? "?" : CanOmitQuotes(value) ? value.ToString()! : $"\"{value}\"";
+            }
+
             public override void AddRow(params object?[] values){
-                writer.Write('"');
-                writer.Write(string.Join("\",\"", ReadRowAsString(values)));
-                writer.Write('"');
+                writer.Write(string.Join(",", ReadRowAsStrings(values)));
                 writer.WriteLine();
                 writer.Flush();
             }
