6b757891319a2a99cabc6c54e8034fc97ac98830
chylex <contact@chylex.com>
2020-03-11 02:55:12 +0100

    Redesign BrotliBuilder serialization parameters & include context map strategies

--- BrotliBuilder/Dialogs/FormSerializationParameters.Designer.cs
+++ BrotliBuilder/Dialogs/FormSerializationParameters.Designer.cs
@@ -23,63 +23,34 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.checkBoxComplexTreeSkipCode = new System.Windows.Forms.CheckBox();
-            this.checkBoxComplexTreeRepeatCode = new System.Windows.Forms.CheckBox();
-            this.groupHuffmanTrees = new System.Windows.Forms.GroupBox();
-            this.groupContextMaps = new System.Windows.Forms.GroupBox();
+            this.groupContextMapsGeneral = new System.Windows.Forms.GroupBox();
             this.checkBoxContextMapMTF = new System.Windows.Forms.CheckBox();
-            this.checkBoxContextMapRLE = new System.Windows.Forms.CheckBox();
             this.buttonReserialize = new System.Windows.Forms.Button();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.groupContextMapsRLE = new System.Windows.Forms.GroupBox();
+            this.radioContextMapsRleSplit1AB = new System.Windows.Forms.RadioButton();
+            this.radioContextMapsRleKeepAll = new System.Windows.Forms.RadioButton();
+            this.radioContextMapsRleDisable = new System.Windows.Forms.RadioButton();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.groupContextMapsGeneral.SuspendLayout();
+            this.tableLayoutPanel.SuspendLayout();
+            this.groupContextMapsRLE.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            this.SuspendLayout();
             // 
-            // checkBoxComplexTreeSkipCode
+            // groupContextMapsGeneral
             // 
-            this.checkBoxComplexTreeSkipCode.AutoSize = true;
-            this.checkBoxComplexTreeSkipCode.Checked = true;
-            this.checkBoxComplexTreeSkipCode.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxComplexTreeSkipCode.Location = new System.Drawing.Point(12, 22);
-            this.checkBoxComplexTreeSkipCode.Margin = new System.Windows.Forms.Padding(9, 3, 3, 3);
-            this.checkBoxComplexTreeSkipCode.Name = "checkBoxComplexTreeSkipCode";
-            this.checkBoxComplexTreeSkipCode.Size = new System.Drawing.Size(176, 19);
-            this.checkBoxComplexTreeSkipCode.TabIndex = 0;
-            this.checkBoxComplexTreeSkipCode.Text = "Use Complex Tree Skip Code";
-            this.checkBoxComplexTreeSkipCode.UseVisualStyleBackColor = true;
+            this.groupContextMapsGeneral.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupContextMapsGeneral.Controls.Add(this.checkBoxContextMapMTF);
+            this.groupContextMapsGeneral.Location = new System.Drawing.Point(3, 3);
+            this.groupContextMapsGeneral.Name = "groupContextMapsGeneral";
+            this.groupContextMapsGeneral.Size = new System.Drawing.Size(254, 51);
+            this.groupContextMapsGeneral.TabIndex = 0;
+            this.groupContextMapsGeneral.TabStop = false;
+            this.groupContextMapsGeneral.Text = "Context Maps — General";
             // 
-            // checkBoxComplexTreeRepeatCode
-            // 
-            this.checkBoxComplexTreeRepeatCode.AutoSize = true;
-            this.checkBoxComplexTreeRepeatCode.Checked = true;
-            this.checkBoxComplexTreeRepeatCode.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxComplexTreeRepeatCode.Location = new System.Drawing.Point(12, 47);
-            this.checkBoxComplexTreeRepeatCode.Margin = new System.Windows.Forms.Padding(9, 3, 3, 3);
-            this.checkBoxComplexTreeRepeatCode.Name = "checkBoxComplexTreeRepeatCode";
-            this.checkBoxComplexTreeRepeatCode.Size = new System.Drawing.Size(190, 19);
-            this.checkBoxComplexTreeRepeatCode.TabIndex = 1;
-            this.checkBoxComplexTreeRepeatCode.Text = "Use Complex Tree Repeat Code";
-            this.checkBoxComplexTreeRepeatCode.UseVisualStyleBackColor = true;
-            // 
-            // groupHuffmanTrees
-            // 
-            this.groupHuffmanTrees.Controls.Add(this.checkBoxComplexTreeSkipCode);
-            this.groupHuffmanTrees.Controls.Add(this.checkBoxComplexTreeRepeatCode);
-            this.groupHuffmanTrees.Location = new System.Drawing.Point(12, 12);
-            this.groupHuffmanTrees.Name = "groupHuffmanTrees";
-            this.groupHuffmanTrees.Size = new System.Drawing.Size(219, 75);
-            this.groupHuffmanTrees.TabIndex = 0;
-            this.groupHuffmanTrees.TabStop = false;
-            this.groupHuffmanTrees.Text = "Huffman Trees";
-            // 
-            // groupContextMaps
-            // 
-            this.groupContextMaps.Controls.Add(this.checkBoxContextMapMTF);
-            this.groupContextMaps.Controls.Add(this.checkBoxContextMapRLE);
-            this.groupContextMaps.Location = new System.Drawing.Point(237, 12);
-            this.groupContextMaps.Name = "groupContextMaps";
-            this.groupContextMaps.Size = new System.Drawing.Size(247, 75);
-            this.groupContextMaps.TabIndex = 1;
-            this.groupContextMaps.TabStop = false;
-            this.groupContextMaps.Text = "Context Maps";
-            // 
-            // checkBoxContextMapIMTF
+            // checkBoxContextMapMTF
             // 
             this.checkBoxContextMapMTF.AutoSize = true;
             this.checkBoxContextMapMTF.Checked = true;
@@ -87,58 +58,130 @@
             this.checkBoxContextMapMTF.Location = new System.Drawing.Point(12, 22);
             this.checkBoxContextMapMTF.Margin = new System.Windows.Forms.Padding(9, 3, 3, 3);
             this.checkBoxContextMapMTF.Name = "checkBoxContextMapMTF";
-            this.checkBoxContextMapMTF.Size = new System.Drawing.Size(226, 19);
+            this.checkBoxContextMapMTF.Size = new System.Drawing.Size(166, 19);
             this.checkBoxContextMapMTF.TabIndex = 0;
-            this.checkBoxContextMapMTF.Text = "Use Move-To-Front Transform";
+            this.checkBoxContextMapMTF.Text = "Move-To-Front Transform";
             this.checkBoxContextMapMTF.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxContextMapRLE
-            // 
-            this.checkBoxContextMapRLE.AutoSize = true;
-            this.checkBoxContextMapRLE.Checked = true;
-            this.checkBoxContextMapRLE.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxContextMapRLE.Location = new System.Drawing.Point(12, 47);
-            this.checkBoxContextMapRLE.Margin = new System.Windows.Forms.Padding(9, 3, 3, 3);
-            this.checkBoxContextMapRLE.Name = "checkBoxContextMapRLE";
-            this.checkBoxContextMapRLE.Size = new System.Drawing.Size(164, 19);
-            this.checkBoxContextMapRLE.TabIndex = 1;
-            this.checkBoxContextMapRLE.Text = "Use Run-Length Encoding";
-            this.checkBoxContextMapRLE.UseVisualStyleBackColor = true;
             // 
             // buttonReserialize
             // 
             this.buttonReserialize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonReserialize.Location = new System.Drawing.Point(391, 93);
+            this.buttonReserialize.Location = new System.Drawing.Point(439, 177);
             this.buttonReserialize.Name = "buttonReserialize";
             this.buttonReserialize.Size = new System.Drawing.Size(93, 23);
-            this.buttonReserialize.TabIndex = 2;
+            this.buttonReserialize.TabIndex = 1;
             this.buttonReserialize.Text = "Reserialize";
             this.buttonReserialize.UseVisualStyleBackColor = true;
-            this.buttonReserialize.Click += new System.EventHandler(this.buttonReserialize_Click);
             // 
-            // FormParameters
+            // tableLayoutPanel
             // 
-            this.ClientSize = new System.Drawing.Size(496, 128);
+            this.tableLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Controls.Add(this.panelRight, 1, 0);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(520, 159);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // groupContextMapsRLE
+            // 
+            this.groupContextMapsRLE.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupContextMapsRLE.Controls.Add(this.radioContextMapsRleSplit1AB);
+            this.groupContextMapsRLE.Controls.Add(this.radioContextMapsRleKeepAll);
+            this.groupContextMapsRLE.Controls.Add(this.radioContextMapsRleDisable);
+            this.groupContextMapsRLE.Location = new System.Drawing.Point(3, 60);
+            this.groupContextMapsRLE.Name = "groupContextMapsRLE";
+            this.groupContextMapsRLE.Size = new System.Drawing.Size(254, 96);
+            this.groupContextMapsRLE.TabIndex = 1;
+            this.groupContextMapsRLE.TabStop = false;
+            this.groupContextMapsRLE.Text = "Context Maps — Run-Length Encoding";
+            // 
+            // radioContextMapsRleSplit1AB
+            // 
+            this.radioContextMapsRleSplit1AB.AutoSize = true;
+            this.radioContextMapsRleSplit1AB.Location = new System.Drawing.Point(12, 67);
+            this.radioContextMapsRleSplit1AB.Margin = new System.Windows.Forms.Padding(9, 2, 3, 2);
+            this.radioContextMapsRleSplit1AB.Name = "radioContextMapsRleSplit1AB";
+            this.radioContextMapsRleSplit1AB.Size = new System.Drawing.Size(164, 19);
+            this.radioContextMapsRleSplit1AB.TabIndex = 2;
+            this.radioContextMapsRleSplit1AB.TabStop = true;
+            this.radioContextMapsRleSplit1AB.Text = "Split One Above Boundary";
+            this.radioContextMapsRleSplit1AB.UseVisualStyleBackColor = true;
+            // 
+            // radioContextMapsRleKeepAll
+            // 
+            this.radioContextMapsRleKeepAll.AutoSize = true;
+            this.radioContextMapsRleKeepAll.Location = new System.Drawing.Point(12, 44);
+            this.radioContextMapsRleKeepAll.Margin = new System.Windows.Forms.Padding(9, 2, 3, 2);
+            this.radioContextMapsRleKeepAll.Name = "radioContextMapsRleKeepAll";
+            this.radioContextMapsRleKeepAll.Size = new System.Drawing.Size(68, 19);
+            this.radioContextMapsRleKeepAll.TabIndex = 1;
+            this.radioContextMapsRleKeepAll.TabStop = true;
+            this.radioContextMapsRleKeepAll.Text = "Keep All";
+            this.radioContextMapsRleKeepAll.UseVisualStyleBackColor = true;
+            // 
+            // radioContextMapsRleDisable
+            // 
+            this.radioContextMapsRleDisable.AutoSize = true;
+            this.radioContextMapsRleDisable.Location = new System.Drawing.Point(12, 21);
+            this.radioContextMapsRleDisable.Margin = new System.Windows.Forms.Padding(9, 2, 3, 2);
+            this.radioContextMapsRleDisable.Name = "radioContextMapsRleDisable";
+            this.radioContextMapsRleDisable.Size = new System.Drawing.Size(63, 19);
+            this.radioContextMapsRleDisable.TabIndex = 0;
+            this.radioContextMapsRleDisable.TabStop = true;
+            this.radioContextMapsRleDisable.Text = "Disable";
+            this.radioContextMapsRleDisable.UseVisualStyleBackColor = true;
+            // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.groupContextMapsRLE);
+            this.panelRight.Controls.Add(this.groupContextMapsGeneral);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight.Location = new System.Drawing.Point(260, 0);
+            this.panelRight.Margin = new System.Windows.Forms.Padding(0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(260, 159);
+            this.panelRight.TabIndex = 1;
+            // 
+            // FormSerializationParameters
+            // 
+            this.ClientSize = new System.Drawing.Size(544, 212);
             this.Controls.Add(this.buttonReserialize);
-            this.Controls.Add(this.groupContextMaps);
-            this.Controls.Add(this.groupHuffmanTrees);
-            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "FormSerializationParameters";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Serialization Parameters";
+            this.groupContextMapsGeneral.ResumeLayout(false);
+            this.groupContextMapsGeneral.PerformLayout();
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.groupContextMapsRLE.ResumeLayout(false);
+            this.groupContextMapsRLE.PerformLayout();
+            this.panelRight.ResumeLayout(false);
+            this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.CheckBox checkBoxComplexTreeSkipCode;
-        private System.Windows.Forms.CheckBox checkBoxComplexTreeRepeatCode;
-        private System.Windows.Forms.GroupBox groupHuffmanTrees;
-        private System.Windows.Forms.GroupBox groupContextMaps;
+        private System.Windows.Forms.GroupBox groupContextMapsGeneral;
         private System.Windows.Forms.CheckBox checkBoxContextMapMTF;
-        private System.Windows.Forms.CheckBox checkBoxContextMapRLE;
         private System.Windows.Forms.Button buttonReserialize;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.GroupBox groupContextMapsRLE;
+        private System.Windows.Forms.RadioButton radioContextMapsRleSplit1AB;
+        private System.Windows.Forms.RadioButton radioContextMapsRleKeepAll;
+        private System.Windows.Forms.RadioButton radioContextMapsRleDisable;
+        private System.Windows.Forms.Panel panelRight;
     }
 }
--- BrotliBuilder/Dialogs/FormSerializationParameters.cs
+++ BrotliBuilder/Dialogs/FormSerializationParameters.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 using BrotliLib.Brotli.Parameters;
 using BrotliLib.Brotli.Parameters.Heuristics;
@@ -10,12 +12,20 @@ namespace BrotliBuilder.Dialogs{
         public event EventHandler? Reserialize;
 
         private readonly Builder parameters;
+        private readonly Dictionary<RadioButton, ContextMapHeuristics.DecideRuns> optionsContextMapsRLE;
 
         public FormSerializationParameters(BrotliSerializationParameters parameters){
             InitializeComponent();
 
             this.parameters = new Builder(parameters);
+            this.buttonReserialize.Click += buttonReserialize_Click;
             this.Disposed += FormParameters_Disposed;
+
+            this.optionsContextMapsRLE = new Dictionary<RadioButton, ContextMapHeuristics.DecideRuns>{
+                { radioContextMapsRleDisable,  ContextMapHeuristics.RLE.Disable },
+                { radioContextMapsRleKeepAll,  ContextMapHeuristics.RLE.KeepAll },
+                { radioContextMapsRleSplit1AB, ContextMapHeuristics.RLE.SplitOneAboveBoundary },
+            };
 
             LoadOptions();
             SetupOptionEvents();
@@ -25,37 +35,37 @@ namespace BrotliBuilder.Dialogs{
             Reserialize = null;
         }
 
-        private void buttonReserialize_Click(object sender, EventArgs e){
+        private void buttonReserialize_Click(object? sender, EventArgs e){
             Reserialize?.Invoke(this, e);
         }
 
         // Option handling
 
-        private static readonly DecideComplexTreeFeature ComplexTreeFeatureEnable  = _ => true;
-        private static readonly DecideComplexTreeFeature ComplexTreeFeatureDisable = _ => false;
-
         private void SetupOptionEvents(){
-            this.checkBoxComplexTreeSkipCode.CheckedChanged   += OnOptionChanged;
-            this.checkBoxComplexTreeRepeatCode.CheckedChanged += OnOptionChanged;
 
             this.checkBoxContextMapMTF.CheckedChanged += OnOptionChanged;
-            this.checkBoxContextMapRLE.CheckedChanged += OnOptionChanged;
+            this.radioContextMapsRleDisable.CheckedChanged += OnOptionChanged;
+            this.radioContextMapsRleKeepAll.CheckedChanged += OnOptionChanged;
+            this.radioContextMapsRleSplit1AB.CheckedChanged += OnOptionChanged;
+        }
+
+        private void ProcessRadio<T>(Dictionary<RadioButton, T> dictionary, Func<KeyValuePair<RadioButton, T>, bool> predicate, Action<KeyValuePair<RadioButton, T>> action){
+            foreach(var kvp in dictionary.Where(predicate)){
+                action(kvp);
+                break;
+            }
         }
 
         private void LoadOptions(){
-            checkBoxComplexTreeSkipCode.Checked   = !ReferenceEquals(parameters.UseComplexTreeSkipCode,   ComplexTreeFeatureDisable);
-            checkBoxComplexTreeRepeatCode.Checked = !ReferenceEquals(parameters.UseComplexTreeRepeatCode, ComplexTreeFeatureDisable);
+            ProcessRadio(optionsContextMapsRLE,  kvp => ReferenceEquals(parameters.ContextMapRLE, kvp.Value),  kvp => kvp.Key.Checked = true);
 
             checkBoxContextMapMTF.Checked = !ReferenceEquals(parameters.ContextMapMTF, ContextMapHeuristics.MTF.Disable);
-            checkBoxContextMapRLE.Checked = !ReferenceEquals(parameters.ContextMapRLE, ContextMapHeuristics.RLE.Disable);
         }
 
         private void OnOptionChanged(object? sender, EventArgs e){
-            parameters.UseComplexTreeSkipCode   = checkBoxComplexTreeSkipCode.Checked   ? ComplexTreeFeatureEnable : ComplexTreeFeatureDisable;
-            parameters.UseComplexTreeRepeatCode = checkBoxComplexTreeRepeatCode.Checked ? ComplexTreeFeatureEnable : ComplexTreeFeatureDisable;
+            ProcessRadio(optionsContextMapsRLE,  kvp => kvp.Key.Checked, kvp => parameters.ContextMapRLE = kvp.Value);
 
             parameters.ContextMapMTF = checkBoxContextMapMTF.Checked ? ContextMapHeuristics.MTF.Enable : ContextMapHeuristics.MTF.Disable;
-            parameters.ContextMapRLE = checkBoxContextMapRLE.Checked ? ContextMapHeuristics.RLE.KeepAll : ContextMapHeuristics.RLE.Disable;
 
             Updated?.Invoke(this, parameters.Build());
         }
