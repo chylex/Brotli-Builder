1183f9fdecab1897ea2bd853538064767bf58469
chylex <contact@chylex.com>
2020-03-19 16:51:10 +0100

    Split BrotliCalc test-reserialize-rebuild command into test-reserialize & test-rebuild

--- BrotliCalc/Commands/CmdTestRebuild.cs
+++ BrotliCalc/Commands/CmdTestRebuild.cs
@@ -5,27 +5,26 @@ using BrotliCalc.Helpers;
 using BrotliImpl.Transformers;
 
 namespace BrotliCalc.Commands{
-    class CmdTestReserializeRebuild : CmdAbstractFileTable.Compressed{
-        public override string FullName => "test-reserialize-rebuild";
-        public override string ShortName => "trr";
+    class CmdTestRebuild : CmdAbstractFileTable.Compressed{
+        public override string FullName => "test-rebuild";
+        public override string ShortName => "trb";
 
         protected override string[] Columns { get; } = {
-            "File", "Quality", "Original Bytes", "Reserialize Bytes", "Rebuild Bytes", "Reserialize-Original", "Rebuild-Original"
+            "File", "Quality", "Original Bytes", "Rebuild Bytes", "Rebuild-Original"
         };
 
         protected override IEnumerable<object?[]> GenerateRows(BrotliFileGroup group, BrotliFile.Compressed file){
             int? originalBytes = file.SizeBytes;
-            var reserializeBytes = group.CountBytesAndValidate(file.Reader);
             var rebuildBytes = group.CountBytesAndValidate(file.Transforming(new TransformRebuild()));
 
             return new List<object?[]>{
-                new object?[]{ file.Name, file.Identifier, originalBytes, reserializeBytes, rebuildBytes, reserializeBytes - originalBytes, rebuildBytes - originalBytes } // subtraction propagates null
+                new object?[]{ file.Name, file.Identifier, originalBytes, rebuildBytes, rebuildBytes - originalBytes } // subtraction propagates null
             };
         }
 
         protected override IEnumerable<object?[]> OnError(BrotliFileGroup group, BrotliFile.Compressed file, Exception ex){
             return new List<object?[]>{
-                new object?[]{ file.Name, file.Identifier, file.SizeBytes, null, null, null, null }
+                new object?[]{ file.Name, file.Identifier, file.SizeBytes, null, null }
             };
         }
     }
---
+++ BrotliCalc/Commands/CmdTestReserialize.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using BrotliCalc.Commands.Base;
+using BrotliCalc.Helpers;
+
+namespace BrotliCalc.Commands{
+    class CmdTestReserialize : CmdAbstractFileTable.Compressed{
+        public override string FullName => "test-reserialize";
+        public override string ShortName => "trs";
+
+        protected override string[] Columns { get; } = {
+            "File", "Quality", "Original Bytes", "Reserialized Bytes", "Reserialized-Original"
+        };
+
+        protected override IEnumerable<object?[]> GenerateRows(BrotliFileGroup group, BrotliFile.Compressed file){
+            int? originalBytes = file.SizeBytes;
+            var reserializeBytes = group.CountBytesAndValidate(file.Reader);
+
+            return new List<object?[]>{
+                new object?[]{ file.Name, file.Identifier, originalBytes, reserializeBytes, reserializeBytes - originalBytes } // subtraction propagates null
+            };
+        }
+
+        protected override IEnumerable<object?[]> OnError(BrotliFileGroup group, BrotliFile.Compressed file, Exception ex){
+            return new List<object?[]>{
+                new object?[]{ file.Name, file.Identifier, file.SizeBytes, null, null }
+            };
+        }
+    }
+}
